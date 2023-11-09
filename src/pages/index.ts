8f05ff2649ca102fae5f4f87711afe66839c820e
prgmr99 <johnyeom24@naver.com>
2023-11-09 23:54:30 +0900

    feat: #6 멤버 페이지 적용 및 헤더와 라우터 연결

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 import MainPage from './MainPage';
 import RecruitPage from './RecruitPage';
 import GalaryPage from './GalaryPage';
+import MemberPage from './MemberPage';
 import InformationUsePage from './InformationUsePage';
 
-export { MainPage, RecruitPage, GalaryPage, InformationUsePage };
+export { MainPage, RecruitPage, MemberPage, GalaryPage, InformationUsePage };
