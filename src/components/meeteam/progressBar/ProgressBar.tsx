a1c3e2e92dbcb48efdfb4dfe65c8195edd7ef090
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2023-11-17 23:34:04 +0900

    fix: style 수정
    이유: 이전 디자인으로 제작 및 enum -> union 으로 변경

--- src/components/meeteam/progressBar/ProgressBar.tsx
+++ src/components/meeteam/progressBar/ProgressBar.tsx
@@ -3,25 +3,20 @@ import S from './ProgressBar.styled';
 
 const ProgressBar = () => {
 	const [status, setStatus] = useState('진행중');
-	enum STATUS {
-		recruit = '구인중',
-		ongoing = '진행중',
-		done = '진행 완료',
-	}
+
+	type STATUS = '진행중' | '진행 완료';
 
 	useEffect(() => {
 		setStatus(status);
 	}, [status]);
 
-	const checkColorChange = (statusType: string) => statusType === status;
+	const checkColorChange = (statusType: STATUS) => statusType === status;
 
 	return (
 		<S.ProgressBarRow>
-			<S.ProgressBarBox color={checkColorChange(STATUS.recruit)}>{STATUS.recruit}</S.ProgressBarBox>
+			<S.ProgressBarBox color={checkColorChange('진행중')}>진행중</S.ProgressBarBox>
 			<S.ProgressBarIcon />
-			<S.ProgressBarBox color={checkColorChange(STATUS.ongoing)}>{STATUS.ongoing}</S.ProgressBarBox>
-			<S.ProgressBarIcon />
-			<S.ProgressBarBox color={checkColorChange(STATUS.done)}>{STATUS.done}</S.ProgressBarBox>
+			<S.ProgressBarBox color={checkColorChange('진행 완료')}>진행 완료</S.ProgressBarBox>
 		</S.ProgressBarRow>
 	);
 };
