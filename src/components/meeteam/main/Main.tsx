c6877212d104b28c64ccf709f9a304d1c0899c60
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2023-11-21 11:24:37 +0900

    style: 밋팀 관리 페이지_진행중 전체 UI 완성

--- src/components/meeteam/main/Main.tsx
+++ src/components/meeteam/main/Main.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import S from './Main.sytled';
-import Informaion from './information/Information';
+import Information from './information/Information';
+import Member from './member/Member';
+import WorkRegistration from './workRegistration/WorkRegistration';
+import Setting from './setting/Setting';
 
 const Main = () => {
+	const [content, setContent] = useState();
+
 	return (
 		<S.MainLayout>
+			<Information />
+			{/* <WorkRegistration /> */}
+			{/* <Member /> */}
+			{/* <Setting /> */}
 		</S.MainLayout>
 	);
 };
