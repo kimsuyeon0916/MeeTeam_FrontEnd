6b95f48fae14c9cc809a8ff6d6fb32c1a81ceb47
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2023-12-29 15:35:33 +0900

    feat: #18 태그와 색깔 매칭 유틸 함수 구현

---
+++ src/utils/ColorMatching.tsx
@@ -0,0 +1,47 @@
+interface colorProps {
+	title: string;
+	color: string;
+}
+
+const colorChart: colorProps[] = [
+	{
+		title: '교내',
+		color: '#FCEFAA',
+	},
+	{
+		title: '프로젝트',
+		color: '#EEECFF',
+	},
+	{
+		title: '오프라인',
+		color: '#E2E2E2',
+	},
+	{
+		title: '개발',
+		color: '#EAF7FF',
+	},
+	{
+		title: '응소실',
+		color: '#EAF7FF',
+	},
+	{
+		title: 'Spring',
+		color: '#E4F9D0',
+	},
+	{
+		title: 'C#',
+		color: '#E0E6FF',
+	},
+	{
+		title: 'Window Form',
+		color: '#F7E8FB',
+	},
+];
+
+const ColorMatching = (content: string) => {
+	const color = colorChart.find(chart => chart.title === content)?.color;
+	if (color === undefined) return '#FFFFFF';
+	return color;
+};
+
+export default ColorMatching;
