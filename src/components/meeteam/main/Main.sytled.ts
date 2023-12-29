336c8de47de26072120fbbecdd8caa028ffbfa56
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2023-12-29 15:54:32 +0900

    style: #18 메인 사이즈 변경 및 테두리 굵기 조절
    - 단위 변경 vw -> rem
    - 굵기 두께 변경

--- src/components/meeteam/main/Main.sytled.ts
+++ src/components/meeteam/main/Main.sytled.ts
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 
 const MainLayout = styled.div`
 	display: flex;
+	font-size: 1.6rem;
 	flex-direction: column;
-	margin-left: 15.052vw;
-	margin-right: 15.052vw;
-	margin-bottom: 6.354vw;
-	height: auto;
+	width: clamp(50%, 108rem, 70%);
+	background-color: #F7FAFF;
+	margin: 0 auto;
+	margin-bottom: 10%;
 	border-radius: 0vw 0vw 0.417vw 0.417vw;
-	border-left: solid #eeecff;
-	border-right: solid #eeecff;
-	border-bottom: solid #eeecff;
-	padding: 2.604vw;
+	border-left: 0.15rem solid #BCD7FF;
+	border-right: 0.15rem solid #BCD7FF;
+	border-bottom: 0.15rem solid #BCD7FF;
+	padding: 3.75rem;
 `;
 
 const S = { MainLayout };
