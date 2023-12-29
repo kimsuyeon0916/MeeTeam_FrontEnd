d69de20a586a26608abd233307c5da3ab922658f
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2023-12-29 21:34:30 +0900

    feat: #18 뒤로가기 컴포넌트 제작

---
+++ src/utils/goBack/GoBack.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import S from './GoBack.styled';
+
+const GoBack = () => {
+	return (
+		<>
+			<S.GoBackButton />
+		</>
+	);
+};
+
+export default GoBack;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
 import modules from './editorModule';
 import ScrollToTop from './ScrollToTop';
+import Toggle from './toggle/Toggle';
+import RandomColor from './RandomColor';
+import ColorMatching from './ColorMatching';
+import GoBack from './goBack/GoBack';
 
-export { modules, ScrollToTop };
+export { modules, ScrollToTop, Toggle, RandomColor, ColorMatching, GoBack };
