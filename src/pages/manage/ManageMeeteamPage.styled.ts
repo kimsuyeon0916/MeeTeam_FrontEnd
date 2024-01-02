b69a31fde0ca96340f1699c5ad5e07d378e2e2f4
prgmr99 <johnyeom24@naver.com>
2024-01-02 18:02:13 +0900

    feat: #23 상태 상자 생성

---
+++ src/pages/manage/ManageMeeteamPage.styled.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const ManageMeeTeamPage = styled.div`
+	.container-status {
+		display: flex;
+		gap: 2.1rem;
+		margin-top: 3.83rem;
+
+		.status {
+			display: flex;
+			width: 17.25rem;
+			height: 4.875rem;
+			padding: 0.75rem;
+			justify-content: center;
+			align-items: center;
+			gap: 0.75rem;
+			flex-shrink: 0;
+			border-radius: 0.6rem;
+			background-color: #5f5cec;
+			color: #fff;
+			font-family: Apple SD Gothic Neo;
+			font-size: 1.7rem;
+			font-style: normal;
+			font-weight: 400;
+			line-height: 1.35rem; /* 75% */
+			letter-spacing: 0.015rem;
+		}
+	}
+`;
+
+const S = { ManageMeeTeamPage };
+
+export default S;
--- src/pages/manage/ManageMeeteamPage.tsx
+++ src/pages/manage/ManageMeeteamPage.tsx
@@ -1,5 +1,17 @@
+import { ProgressBar } from '../../components';
+import S from './ManageMeeteamPage.styled';
+
 const ManageMeeteamPage = () => {
-	return <h1>전체 밋팀 관리 페이지</h1>;
+	return (
+		<S.ManageMeeTeamPage>
+			<div className='container-status'>
+				<div className='status'>구인중</div>
+				<div className='status'>진행중</div>
+				<div className='status'>진행 완료</div>
+			</div>
+			<div></div>
+		</S.ManageMeeTeamPage>
+	);
 };
 
 export default ManageMeeteamPage;
