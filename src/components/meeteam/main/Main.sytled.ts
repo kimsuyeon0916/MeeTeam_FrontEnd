52404c03d53dce01c6893c18583010c0ab94c6f2
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-03 20:53:27 +0900

    style: #18 Main 내 공통 스타일 추가

--- src/components/meeteam/main/Main.sytled.ts
+++ src/components/meeteam/main/Main.sytled.ts
@@ -5,16 +5,52 @@ const MainLayout = styled.div`
 	font-size: 1.6rem;
 	flex-direction: column;
 	width: clamp(50%, 108rem, 70%);
-	background-color: #F7FAFF;
+	background-color: #f7faff;
 	margin: 0 auto;
 	margin-bottom: 10%;
 	border-radius: 0vw 0vw 0.417vw 0.417vw;
-	border-left: 0.15rem solid #BCD7FF;
-	border-right: 0.15rem solid #BCD7FF;
-	border-bottom: 0.15rem solid #BCD7FF;
+	border: 0.15rem solid #bcd7ff;
+	border-top: 0;
 	padding: 3.75rem;
 `;
 
-const S = { MainLayout };
+const MainArticle = styled.article`
+	display: flex;
+	border-radius: 0.75rem;
+	border: 0.075rem solid #bcd7ff;
+	background: #fff;
+	color: var(--Light-Black, #373f41);
+	font-style: normal;
+	font-family: Pretendard;
+
+	.main__column {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.main__row {
+		display: flex;
+		align-items: center;
+	}
+
+	.main--small-text {
+		font-size: 1.2rem;
+		font-weight: 400;
+		line-height: 1.44rem;
+	}
+
+	.main--middle-text {
+		font-size: 1.5rem;
+		font-weight: 400;
+	}
+
+	.main--big-text {
+		color: var(--text-color, #151515);
+		font-size: 1.8rem;
+		font-weight: 500;
+	}
+`;
+
+const S = { MainLayout, MainArticle };
 
 export default S;
