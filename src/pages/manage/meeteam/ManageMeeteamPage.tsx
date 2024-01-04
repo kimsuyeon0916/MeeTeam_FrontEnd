93fd63b4cd3fa6d0c3fb2ddc4ed8909ae269d333
prgmr99 <johnyeom24@naver.com>
2024-01-04 15:54:34 +0900

    feat: #23 상태박스 기능 구현

--- src/pages/manage/meeteam/ManageMeeteamPage.tsx
+++ src/pages/manage/meeteam/ManageMeeteamPage.tsx
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
 import { Card, Status } from '../../../components';
 import S from './ManageMeeteamPage.styled';
 
 const ManageMeeteamPage = () => {
+	const [isClicked, setIsClicked] = useState({
+		isRecruit: true,
+		isProceed: false,
+		isDone: false,
+	});
+
+	const onClickHandler = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		if (event.target.innerText === '구인중') {
+			setIsClicked({ isRecruit: true, isProceed: false, isDone: false });
+		}
+		if (event.target.innerText === '진행중') {
+			setIsClicked({ isRecruit: false, isProceed: true, isDone: false });
+		}
+		if (event.target.innerText === '진행 완료') {
+			setIsClicked({ isRecruit: false, isProceed: false, isDone: true });
+		}
+	};
+
 	return (
 		<S.ManageMeeTeamPage>
 			<div className='container-status'>
-				<Status status='구인중' $check={true} />
-				<Status status='진행중' $check={false} />
-				<Status status='진행 완료' $check={false} />
+				<Status status='구인중' $check={isClicked.isRecruit} onClick={onClickHandler} />
+				<Status status='진행중' $check={isClicked.isProceed} onClick={onClickHandler} />
+				<Status status='진행 완료' $check={isClicked.isDone} onClick={onClickHandler} />
 			</div>
 			<div className='container-filter'>
 				<div className='container-filter_area'>
