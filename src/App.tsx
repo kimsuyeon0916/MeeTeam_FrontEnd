fa820044c25b903212e94c619687094472ef04ac
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-05 14:11:23 +0900

    feat: #18 Recoil 추가
    - 이유: 대시보드 버튼 클릭해서, 밋팀 관리 탭 컴포넌트 렌더링을 하기 위함

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/index';
 import { ScrollToTop } from './utils/index';
+import { RecoilRoot } from 'recoil';
 
 function App() {
 	return (
 		<>
-			<Header />
-			<main>
-				<ScrollToTop />
-				<Outlet />
-			</main>
+			<RecoilRoot>
+				<Header />
+				<main>
+					<ScrollToTop />
+					<Outlet />
+				</main>
+			</RecoilRoot>
 		</>
 	);
 }
