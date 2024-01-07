7d6496de5786ed8b3cbceb6dfee56c4e64759f2c
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-07 17:42:09 +0900

    style: #18 원형 프로필 컴포넌트 스타일 변경
    - 이유: DashBoardMember 컴포넌트에만 사용되는 스타일이 추가되있었음

--- src/utils/profile/RadiusProfile.styled.ts
+++ src/utils/profile/RadiusProfile.styled.ts
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
-interface RadiusProfileProps {
-	$size?: string;
-	$index?: number;
-}
-const RadiusProfileLayout = styled.div<RadiusProfileProps>`
-	position: ${props => (props.$index === 0 ? '' : 'absolute')};
-	left: ${props => 4.3 + 2.5 * props.$index}rem;
+const RadiusProfileLayout = styled.div<{ $size?: string }>`
 	width: ${props =>
 		props.$size === 'big' ? '7.0873rem' : props.$size === 'middle' ? '5.5125rem' : '4.3313rem'};
 	height: ${props =>
@@ -14,7 +8,6 @@ const RadiusProfileLayout = styled.div<RadiusProfileProps>`
 	border-radius: 70%;
 	overflow: hidden;
 	border: 0.075rem solid #b9b9b9;
-	z-index: ${props => props.$index};
 `;
 
 const RadiusProfileImage = styled.img`
--- src/utils/profile/RadiusProfile.tsx
+++ src/utils/profile/RadiusProfile.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import S from './RadiusProfile.styled';
-import { memberProfileDefaultIcon } from '../../components/index';
+import { MEMBER_PROFILE_DEFAULT_ICON } from '../../components/index';
 
-const RadiusProfile = (props: { size?: string; url?: string; index?: number }) => {
+const RadiusProfile = (props: { size?: string; url?: string }) => {
 	return (
-		<S.RadiusProfileLayout $index={props.index} $size={props.size}>
-			{props.url === '' ? (
-				memberProfileDefaultIcon
-			) : (
-				<S.RadiusProfileImage src={props.url} />
-			)}
+		<S.RadiusProfileLayout $size={props.size}>
+			{props.url === '' ? MEMBER_PROFILE_DEFAULT_ICON : <S.RadiusProfileImage src={props.url} />}
 		</S.RadiusProfileLayout>
 	);
 };
