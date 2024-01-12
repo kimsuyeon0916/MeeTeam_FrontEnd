bb09ef198140dee865bfca6b4232a0c1a59a030c
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-12 15:16:12 +0900

    style: #18 옵션 모달 높이 고정 제거

--- src/utils/option/optionModal/OptionModal.styled.ts
+++ src/utils/option/optionModal/OptionModal.styled.ts
@@ -9,7 +9,6 @@ const OptionModalLayout = styled.ul`
 	row-gap: 0.75rem;
 	padding: 0.75rem 0.53rem;
 	width: 7.95rem;
-	height: 4.2rem;
 	border-radius: 0.6rem;
 	background: #fff;
 	box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.1);
