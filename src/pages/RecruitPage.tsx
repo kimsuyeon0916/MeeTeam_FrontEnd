b1a457ce7a5b451a6046f3c7efea853478cabad0
prgmr99 <johnyeom24@naver.com>
2024-01-12 23:34:35 +0900

    feat: #28 구인글 메인페이지 UI 레이아웃

--- src/pages/RecruitPage.tsx
+++ src/pages/RecruitPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Dropdown } from '../components';
+import { Dropdown, Tag } from '../components';
 import S from './RecruitPage.styled';
 
 const RecruitPage = () => {
@@ -71,9 +71,145 @@ const RecruitPage = () => {
 						</div>
 					</div>
 				</div>
-				<div>
-					<div></div>
-					<div></div>
+				<div className='container-contents'>
+					<div className='container-contents__row'>
+						<div className='subtitle'>👀 내가 관심 있을 만한 프로젝트</div>
+						<div className='contents'>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='container-contents__row'>
+						<div className='subtitle'>👀 내가 관심 있을 만한 프로젝트</div>
+						<div className='contents'>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+							<div className='content'>
+								<div className='content-tags'>
+									<div className='tags'>
+										<div>교외</div>
+										<div>프로젝트</div>
+									</div>
+									<Tag $recruit={true} $proceed={false}></Tag>
+								</div>
+								<div className='content-title'>
+									[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
+								</div>
+								<div className='content-info'>
+									<div>🚨 마감 7일 전</div>
+									<div>조회수 101회</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</S.RecruitPage>
