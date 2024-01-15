07de600413607e6d2015a907afc9b1c0d62cd5d3
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-15 14:02:39 +0900

    feat: #18 구인 시작 버튼/구인 마감일 컴포넌트 제작 및 스타일 추가

---
+++ src/components/meeteam/main/recruitment/RecruitmentDeadLine.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import S from './Recruitment.styeld';
+import { recruitmentInformation } from '../../..';
+import { useRecoilState } from 'recoil';
+import { recruitmentInformationEditState, recruitmentState } from '../../../../atom';
+
+const RecruitmentDeadLine = () => {
+	const [editMode, setEditMode] = useState(false);
+
+	const year = recruitmentInformation?.deadLine?.getFullYear();
+	const month = (recruitmentInformation?.deadLine?.getMonth() + 1).toString().padStart(2, '0');
+	const date = recruitmentInformation?.deadLine?.getDate().toString().padStart(2, '0');
+
+	const [recruitment, setRecruitment] = useRecoilState(recruitmentState);
+	const [recruitmentInformationEdit, setRecruitmentInformationEdit] = useRecoilState(
+		recruitmentInformationEditState
+	);
+
+	const startRecruit = () => {
+		setRecruitment(true);
+		setEditMode(true);
+		setRecruitmentInformationEdit(true);
+	};
+
+	return (
+		<S.RecruitmentArticle>
+			<h2 className='main--big-text'>{recruitment ? '마감일' : '구인'}</h2>
+			{recruitment ? (
+				editMode ? (
+					<>
+						<div className='recruitment__button-row'>
+							<S.RecruitmentCancelButton type='button' onClick={() => setEditMode(false)}>
+								취소
+							</S.RecruitmentCancelButton>
+							<S.RecruitmentSaveButton onClick={() => setEditMode(false)}>
+								저장하기
+							</S.RecruitmentSaveButton>
+						</div>
+					</>
+				) : (
+					<>
+						<div>
+							~ {year}.{month}.{date}
+						</div>
+						<div className='recruitment__button-row'>
+							<S.RecruitmentCorrectButton type='button' onClick={() => setEditMode(true)}>
+								수정 하기
+							</S.RecruitmentCorrectButton>
+							<S.RecruitmentEndButton>구인 종료</S.RecruitmentEndButton>
+						</div>
+					</>
+				)
+			) : (
+				<>
+					<div>아직까지 함께 할 사람들을 못찾았다면? 함께 할 사람들을 찾을 수 있습니다!</div>
+					<div className='recruitment__button-row'>
+						<S.RecruitmentButton type='button' onClick={startRecruit}>
+							구인하기
+						</S.RecruitmentButton>
+					</div>
+				</>
+			)}
+		</S.RecruitmentArticle>
+	);
+};
+
+export default RecruitmentDeadLine;
