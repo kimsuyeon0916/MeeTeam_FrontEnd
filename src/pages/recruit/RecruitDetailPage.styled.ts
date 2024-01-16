e0fd57a67f05253a55d7f0635429841065a67184
prgmr99 <johnyeom24@naver.com>
2024-01-16 17:06:04 +0900

    style: #33 구인글 상세페이지 정보 UI 작업

--- src/pages/recruit/RecruitDetailPage.styled.ts
+++ src/pages/recruit/RecruitDetailPage.styled.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+interface Props {
+	$isRound: boolean;
+	$color: string;
+}
+
 const RecruitDetailPage = styled.div`
 	width: clamp(45%, 108rem, 75%);
 	margin: 0 auto;
@@ -21,7 +26,86 @@ const RecruitDetailPage = styled.div`
 				border-radius: 0.75rem;
 				border: 1.5px solid #eeecff;
 				background: #fff;
+				padding: 2.25rem 3.45rem;
+				box-sizing: border-box;
+
+				.container-info__title {
+					display: flex;
+					align-items: center;
+					gap: 1.27rem;
+					margin-top: 0.5rem;
+
+					h1 {
+						margin-top: 0.2rem;
+						color: #000;
+						font-family: Apple SD Gothic Neo;
+						font-size: 2.4rem;
+						font-style: normal;
+						font-weight: 400;
+						line-height: 3rem;
+						letter-spacing: 0.015rem;
+					}
+				}
+
+				.container-info__writer {
+					display: flex;
+					align-items: center;
+					gap: 0.7rem;
+					margin-top: 1.37rem;
+
+					.profile-img {
+						width: 3.3075rem;
+						height: 3.3075rem;
+						flex-shrink: 0;
+						background-color: #bcd7ff;
+						border-radius: 50%;
+					}
+
+					div:nth-child(2) {
+						color: var(--Light-Black, var(--text-color-2, #373f41));
+						font-family: Apple SD Gothic Neo;
+						font-size: 1.5rem;
+						font-style: normal;
+						font-weight: 400;
+						line-height: 1.35rem; /* 90% */
+						letter-spacing: 0.015rem;
+					}
+				}
 			}
+
+			.container-required__info {
+				margin-top: 3.6rem;
+				display: grid;
+				grid-template-columns: 1fr 2fr;
+				grid-row-gap: 1.5rem;
+				grid-column-gap: 8rem;
+			}
+
+			.container-introduction {
+				margin-top: 5rem;
+
+				h4 {
+					color: #000;
+					font-family: Apple SD Gothic Neo;
+					font-size: 1.8rem;
+					font-style: normal;
+					font-weight: 500;
+					line-height: 1.8rem; /* 100% */
+					letter-spacing: -0.036rem;
+				}
+
+				p {
+					margin-top: 2rem;
+					color: var(--text-color-2, #373f41);
+					font-family: Pretendard;
+					font-size: 1.5rem;
+					font-style: normal;
+					font-weight: 400;
+					line-height: 150%; /* 2.7rem */
+					letter-spacing: 0.015rem;
+				}
+			}
+
 			.container-current {
 				height: 26.1rem;
 				flex-shrink: 0;
@@ -70,6 +154,40 @@ const RecruitDetailPage = styled.div`
 	}
 `;
 
-const S = { RecruitDetailPage };
+const RequiredInformationItem = styled.li`
+	display: flex;
+	column-gap: 2.25rem;
+	align-items: center;
+	.required-information__row {
+		display: flex;
+		column-gap: 1.2rem;
+	}
+`;
 
-export default S;
+const RequiredInformationHead = styled.h3`
+	display: flex;
+	color: var(--text-color, #151515);
+	font-weight: 400;
+	font-size: 1.4rem;
+`;
+
+const RequiredInformationSpan = styled.span<Props>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0.75rem;
+	height: 3.15rem;
+	font-size: 1.4rem;
+	font-weight: 400;
+	border-radius: ${props => (props.$isRound ? `7.5rem` : `0.6rem`)};
+	background: ${props => props.$color};
+`;
+
+const SRecruit = {
+	RecruitDetailPage,
+	RequiredInformationItem,
+	RequiredInformationSpan,
+	RequiredInformationHead,
+};
+
+export default SRecruit;
--- src/pages/recruit/RecruitDetailPage.tsx
+++ src/pages/recruit/RecruitDetailPage.tsx
@@ -1,15 +1,96 @@
 import React from 'react';
-import S from './RecruitDetailPage.styled';
+import SRecruit from './RecruitDetailPage.styled';
 import { useLocation } from 'react-router-dom';
+import { Tag } from '../../components';
+import ColorMatching from '../../utils/ColorMatching';
+
+interface RequiredInformation {
+	title: string;
+	content: string;
+}
 
 const RecruitDetailPage = () => {
 	const location = useLocation();
-	console.log(location);
+
+	const informationList: RequiredInformation[] = [
+		{
+			title: '범위',
+			content: '교내',
+		},
+		{
+			title: '유형',
+			content: '프로젝트',
+		},
+		{
+			title: '진행',
+			content: '오프라인',
+		},
+		{
+			title: '분야',
+			content: '개발',
+		},
+		{
+			title: '기간',
+			content: '23.10.23 - 24.02.14',
+		},
+		{
+			title: '태그',
+			content: '응소실,Spring,C#,Window Form',
+		},
+	];
+
+	const CONTENT =
+		'밋팀(Meeteam)은 나 자신을 의미하는 Me, 팀을 의미하는 Team, 만남을 의미하는 Meet이 합쳐진 단어입니다. 대학생들의 보다 원활한 팀프로젝트를 위해 기획하게 되었으며, 그 외에 포토폴리오로서의 기능까지 생각하고 있습니다! 이를 위해 함께 멋진 서비스를 완성할 웹 디자이너를 찾고 있어요!';
+
+	const isRound = (title: string) => {
+		const roundTitles = ['유형', '진행'];
+
+		if (roundTitles.includes(title)) {
+			return false;
+		}
+		return true;
+	};
+
 	return (
-		<S.RecruitDetailPage>
+		<SRecruit.RecruitDetailPage>
 			<div className='container'>
 				<div className='container-left'>
-					<div className='container-info'></div>
+					<div className='container-info'>
+						<div>
+							<div className='container-info__title'>
+								<h1>[커뮤니티 웹 서비스 프로젝트] 디자이너 모집</h1>
+								<Tag $recruit={true} $proceed={false} />
+							</div>
+							<div className='container-info__writer'>
+								<div className='profile-img'></div>
+								<div>{'김민지'}</div>
+							</div>
+						</div>
+						<div className='container-required__info'>
+							{informationList.map((information, index) => (
+								<SRecruit.RequiredInformationItem key={index}>
+									<SRecruit.RequiredInformationHead>
+										{information.title}
+									</SRecruit.RequiredInformationHead>
+									<div className='required-information__row'>
+										{information.content.split(',').map((content, index) => (
+											<SRecruit.RequiredInformationSpan
+												$isRound={isRound(information.title)}
+												$color={ColorMatching(content)}
+												key={index}
+											>
+												{content}
+											</SRecruit.RequiredInformationSpan>
+										))}
+									</div>
+								</SRecruit.RequiredInformationItem>
+							))}
+						</div>
+						<div className='container-introduction'>
+							<h4>구인 글</h4>
+							<p>{CONTENT}</p>
+						</div>
+					</div>
 					<div className='container-current'></div>
 					<div className='container-tags'></div>
 				</div>
@@ -19,7 +100,7 @@ const RecruitDetailPage = () => {
 				</div>
 			</div>
 			<div className='container-comments'></div>
-		</S.RecruitDetailPage>
+		</SRecruit.RecruitDetailPage>
 	);
 };
 
