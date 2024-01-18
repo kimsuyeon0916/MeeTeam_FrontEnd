257f921902e5708b16df0db2195f473d7a598773
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-18 20:28:08 +0900

    chore: #37 설정 컴포넌트 내 밋팀 중단 버튼 명 변경
    - 밋팀 중단 -> 밋팀 삭제

--- src/components/meeteam/main/setting/Setting.tsx
+++ src/components/meeteam/main/setting/Setting.tsx
@@ -12,9 +12,9 @@ const Setting = () => {
 				<S.SettingButton>공개로 설정하기</S.SettingButton>
 			</S.SettingArticle>
 			<S.SettingArticle>
-				<h2 className='main--big-text'>밋팀 중단</h2>
-				<div>해당 밋팀을 삭제하고 싶으시면 "밋팀 중단"을 눌러주세요.</div>
-				<S.SettingButton $color='#FF6A6A'>밋팀 중단</S.SettingButton>
+				<h2 className='main--big-text'>밋팀 삭제</h2>
+				<div>해당 밋팀을 삭제하고 싶으시면 "밋팀 삭제"을 눌러주세요.</div>
+				<S.SettingButton $color='#FF6A6A'>밋팀 삭제</S.SettingButton>
 			</S.SettingArticle>
 		</S.SettingLayout>
 	);
