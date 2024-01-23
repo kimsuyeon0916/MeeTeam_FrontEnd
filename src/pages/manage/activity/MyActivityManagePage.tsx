87cd8475d76259aa7629c640e26b0b53f0ccf76d
prgmr99 <johnyeom24@naver.com>
2024-01-23 15:16:20 +0900

    chore: #42 북마크한 밋팀 제거

--- src/pages/manage/activity/MyActivityManagePage.tsx
+++ src/pages/manage/activity/MyActivityManagePage.tsx
@@ -7,29 +7,6 @@ const MyActivityManagePage = () => {
 		<S.MyActivityManagePage>
 			<div className='container-contents'>
 				<div className='container-contents__row'>
-					<div className='subtitle'>📕 북마크한 밋팀</div>
-					<div className='contents'>
-						<div className='content'>
-							<Card />
-							<div className='title'>
-								[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
-							</div>
-						</div>
-						<div className='content'>
-							<Card />
-							<div className='title'>
-								[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
-							</div>
-						</div>
-						<div className='content'>
-							<Card />
-							<div className='title'>
-								[반려 동물을 위한 ~ 팀 프로젝트] 개발자를 모집합니다. 프로그래밍 스터디 진행
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className='container-contents__row'>
 					<div className='subtitle'>👀 제의받은 밋팀</div>
 					<div className='contents'>
 						<div className='content'>
