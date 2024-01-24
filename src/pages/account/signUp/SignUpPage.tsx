411436e3857d0ecb0ef9175055f1d8eb935d846b
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-24 21:09:29 +0900

    feat: #36 회원가입 페이지 제작 및 스타일 추가

--- src/pages/account/signUp/SignUpPage.tsx
+++ src/pages/account/signUp/SignUpPage.tsx
@@ -1,12 +1,40 @@
-import React from "react";
-import S from "./SignUpPage.styled";
+import React from 'react';
+import S from './SignUpPage.styled';
+import { SIGN_UP_DATA } from '../../index';
 
 const SignUpPage = () => {
-  return (
-    <S.SignUpPageLayout>
-      
-    </S.SignUpPageLayout>
-  )
-}
+	const submitHandelr = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+
+	return (
+		<S.SignUpPageLayout>
+			<header className='account__header'>
+				<h1>회원가입</h1>
+			</header>
+			<S.SignUpPageForm onSubmit={e => submitHandelr(e)}>
+				<div className='account__form-row'>
+					{SIGN_UP_DATA.map(
+						({ label, type, placeholder, name }, index) =>
+							name !== 'nickName' && (
+								<label className='account__label' key={index}>
+									{label}
+									<input
+										className='account__input'
+										type={type}
+										placeholder={placeholder}
+										name={name}
+									></input>
+								</label>
+							)
+					)}
+				</div>
+				<S.SignUpButton type='submit' value='submit'>
+					완료
+				</S.SignUpButton>
+			</S.SignUpPageForm>
+		</S.SignUpPageLayout>
+	);
+};
 
 export default SignUpPage;
