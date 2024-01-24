a5a8a64f96e5d5b8bed95c8e1ef0eac1cca2806f
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-24 14:53:14 +0900

    feat: #36 학사 인증 페이지 UI 구성을 위한 상수 데이터 제작

---
+++ src/pages/schoolCertification/SchoolCertificationData.ts
@@ -0,0 +1,40 @@
+interface School {
+	school: string;
+	major: string;
+	year: string;
+	email: string;
+}
+export type { School };
+
+const SCHOOL_CERTIFICATION_DATA = [
+	{
+		label: '학교',
+		type: 'search',
+		placeholder: '학교',
+		name: 'school',
+		isNext: false,
+	},
+	{
+		label: '학과',
+		type: 'school',
+		placeholder: '학과',
+		name: 'major',
+		isNext: false,
+	},
+	{
+		label: '입학년도',
+		type: 'number',
+		placeholder: 'ex) 2023',
+		name: 'year',
+		isNext: true,
+	},
+	{
+		label: '학교 이메일',
+		type: 'email',
+		placeholder: '학교 이메일을 입력하세요',
+		name: 'email',
+		isNext: true,
+	},
+];
+
+export { SCHOOL_CERTIFICATION_DATA };
