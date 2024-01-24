0aa59914339d8e6cc71158e12f39cdbc7e59ef5b
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-24 20:10:45 +0900

    chore: #36 닉네임 입력 페이지를 공통 스타일 컴포넌트로 변경

--- src/pages/account/signUp/nickNameSetting/NickNameSettingPage.styled.ts
+++ src/pages/account/signUp/nickNameSetting/NickNameSettingPage.styled.ts
@@ -1,58 +1,16 @@
 import styled from 'styled-components';
-import SignIn from '../../signIn/SignInPage.styled';
+import Account from '../../Account.styled';
 
-const NickNameSettingPageLayout = styled.div`
-	display: flex;
-	flex-direction: column;
-	row-gap: 7.5rem;
-	align-items: center;
-	justify-content: center;
-	margin: 15rem auto;
-	letter-spacing: 0.015rem;
-	font-weight: 400;
-	color: var(--text-color-2, #373f41);
-	font-size: 1.5rem;
-
-	.nick-name-setting__header {
-		position: relative;
-		display: flex;
-		line-height: 4.2rem;
-		color: var(--text-color, #151515);
-		font-size: 2.7rem;
-	}
-
+const NickNameSettingPageLayout = styled(Account.AccountLayout)`
 	h1 {
 		text-align: center;
 		white-space: pre-wrap;
 	}
 `;
 
-const NickNameSettingPageForm = styled.form`
-	display: flex;
-	flex-direction: column;
-	row-gap: 10.5rem;
-	width: 34.8rem;
+const NickNameSettingPageForm = styled(Account.AccountForm)``;
 
-	.nick-name-setting__label {
-		display: flex;
-		flex-direction: column;
-		row-gap: 0.81rem;
-	}
-
-	.nick-name-setting__input {
-		all: unset;
-		display: flex;
-		flex: 1;
-		align-items: center;
-		padding: 1.5rem;
-		border-radius: 0.75rem;
-		border: 0.75px solid var(--box_stroke, #e3e3e3);
-		background: #f9f9f9;
-		cursor: text;
-	}
-`;
-
-const NickNameSettingButton = styled(SignIn.SignInPageButton)`
+const NickNameSettingButton = styled(Account.AccountButton)`
 	width: 16.5rem;
 	margin: 0 auto;
 `;
--- src/pages/account/signUp/nickNameSetting/NickNameSettingPage.tsx
+++ src/pages/account/signUp/nickNameSetting/NickNameSettingPage.tsx
@@ -6,16 +6,16 @@ const NickNameSettingPage = () => {
 	const title = `밋팀에서 사용하실\n닉네임을 설정해주세요`;
 	return (
 		<S.NickNameSettingPageLayout>
-			<header className='nick-name-setting__header'>
+			<header className='account__header'>
 				<h1>{title}</h1>
 			</header>
 			<S.NickNameSettingPageForm>
 				{SIGN_UP_DATA.map(
 					({ type, placeholder, name }, index) =>
 						name === 'nickName' && (
-							<label className='nick-name-setting__label' key={index}>
+							<label className='account__label' key={index}>
 								<input
-									className='nick-name-setting__input'
+									className='account__input'
 									type={type}
 									placeholder={placeholder}
 									name={name}
