b6552aa5ddf8edb60b4b1b024877d90c190e9609
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-24 14:49:58 +0900

    feat: #36 뒤로 가기 컴포넌트 props 설정 및 구조 변경
    - 이유: 다른 컴포넌트/페이지 에서 재활용 하기 위함

--- src/components/goBack/GoBack.tsx
+++ src/components/goBack/GoBack.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import S from './GoBack.styled';
 import { useNavigate } from 'react-router-dom';
 
-const GoBack = () => {
+const GoBack = ({
+	clickHandler,
+	style,
+}: {
+	clickHandler?: React.MouseEventHandler<SVGAElement>;
+	style?: string;
+}) => {
 	const navigate = useNavigate();
 
 	return (
 		<>
 			<S.GoBackButton
 				type='button'
-				onClick={() => {
-					navigate(-1);
-				}}
+				onClick={clickHandler ? clickHandler : () => navigate(-1)}
+				$style={style}
 			/>
 		</>
 	);
