d7859734edea201ab70fff74810f8c9602c5bd59
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-25 16:26:50 +0900

    feat: #44 네이버 로그인 컴포넌트 내 커스텀 로그인 버튼 로직 추가

--- src/components/naver/NaverLogin.styled.ts
+++ src/components/naver/NaverLogin.styled.ts
@@ -1,11 +1,35 @@
 import styled from 'styled-components';
 
 const NaverLoginButton = styled.button`
-  all: unset;
-  position: absolute;
+	all: unset;
+	position: absolute;
 	visibility: none;
 `;
 
-const S = { NaverLoginButton };
+const NaverCustomButton = styled.button`
+	all: unset;
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 4.875rem;
+	background: #03c75a;
+	border-radius: 0.45rem;
+	color: #fff;
+	font-size: 1.5rem;
+	font-weight: 500;
+	line-height: 1.35rem;
+	cursor: pointer;
+
+	.sign-in__naver-icon {
+		position: absolute;
+		display: flex;
+		height: 4.875rem;
+		left: 0;
+	}
+`;
+
+const S = { NaverLoginButton, NaverCustomButton };
 
 export default S;
--- src/components/naver/NaverLogin.tsx
+++ src/components/naver/NaverLogin.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import S from './NaverLogin.styled';
 
-const NaverLogin = ({ naverRef }: { naverRef?: React.ForwardedRef<HTMLButtonElement> }) => {
-	return <S.NaverLoginButton ref={naverRef} id='naverIdLogin' />;
+const NaverLogin = () => {
+	const naverRef = useRef<HTMLButtonElement | null>(null);
+
+	const { naver }: any = window;
+
+	const initializeNaverLogin = () => {
+		const naverLogin = new naver.LoginWithNaverId({
+			clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
+			callbackUrl: import.meta.env.VITE_NAVER_CALLBACK_URL,
+			isPopup: false,
+			loginButton: { type: 1, height: '48.75' },
+		});
+
+		naverLogin.init();
+	};
+
+	const naverLoginHandler = () => {
+		const target = naverRef.current && (naverRef.current.children[0] as HTMLDivElement);
+		target && target.click();
+	};
+
+	useEffect(() => {
+		initializeNaverLogin();
+	}, []);
+
+	return (
+		<>
+			<S.NaverLoginButton ref={naverRef} id='naverIdLogin' />
+			<S.NaverCustomButton onClick={naverLoginHandler}>
+				<img src='/src/assets/NaverIcon.png' className='sign-in__naver-icon' alt='naverIcon' />
+				<span>네이버 로그인</span>
+			</S.NaverCustomButton>
+		</>
+	);
 };
 
 export default NaverLogin;
