3bd9d48c20871e24a6e808c798911463024ddc58
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-25 22:54:11 +0900

    refactor: #44 네이버 연동 로직 변경
    - 네이버 아이디 연동 sdk 제거하고 직접 구현

--- src/components/naver/NaverLogin.tsx
+++ src/components/naver/NaverLogin.tsx
@@ -1,39 +1,32 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import S from './NaverLogin.styled';
 
 const NaverLogin = () => {
-	const naverRef = useRef<HTMLButtonElement | null>(null);
-
-	const { naver }: any = window;
-
 	const initializeNaverLogin = () => {
-		const naverLogin = new naver.LoginWithNaverId({
-			clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
-			callbackUrl: import.meta.env.VITE_NAVER_CALLBACK_URL,
-			isPopup: false,
-			loginButton: { type: 1, height: '48.75' },
-		});
+		const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+			import.meta.env.VITE_NAVER_CLIENT_ID
+		}&state=${import.meta.env.VITE_NAVER_STATE}&redirect_uri=${
+			import.meta.env.VITE_NAVER_CALLBACK_URL
+		}`;
 
-		naverLogin.init();
+		window.location.href = NAVER_AUTH_URL;
 	};
 
-	const naverLoginHandler = () => {
-		const target = naverRef.current && (naverRef.current.children[0] as HTMLDivElement);
-		target && target.click();
+	const getAuthCode = () => {
+		const urlParams = new URLSearchParams(window.location.search);
+		return urlParams.get('code');
 	};
 
 	useEffect(() => {
-		initializeNaverLogin();
+		const code = getAuthCode();
 	}, []);
 
+
 	return (
-		<>
-			<S.NaverLoginButton ref={naverRef} id='naverIdLogin' />
-			<S.NaverCustomButton onClick={naverLoginHandler}>
-				<img src='/src/assets/NaverIcon.png' className='sign-in__naver-icon' alt='naverIcon' />
-				<span>네이버 로그인</span>
-			</S.NaverCustomButton>
-		</>
+		<S.NaverCustomButton onClick={initializeNaverLogin}>
+			<img src='/src/assets/NaverIcon.png' className='sign-in__naver-icon' alt='naverIcon' />
+			<span>네이버 로그인</span>
+		</S.NaverCustomButton>
 	);
 };
 
