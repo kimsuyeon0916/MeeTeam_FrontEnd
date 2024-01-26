2e2793e2d4d8173e38a39cb7347b2bca7465f1b7
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-26 16:02:47 +0900

    feat: #44 커스텀 Axios Instance 생성
    - 목적: api 모듈화

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import type { SignUpPayload } from './payload';
+import type { UserReponse, User } from './response';
+import type { CustomInstance } from './api';
+
+export { SignUpPayload, UserReponse, User, CustomInstance };
