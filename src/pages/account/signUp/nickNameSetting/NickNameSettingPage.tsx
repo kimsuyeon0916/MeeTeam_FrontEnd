c1f260ba86e184813c321dcdafbad0b5eaf0d86c
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-26 17:31:34 +0900

    feat: #44 닉네임 설정 확인 버튼 클릭 시 fetch 적용 및 네이버 연동 회원 가입 완료
    - 메인 화면으로 리다이렉션

--- src/pages/account/signUp/nickNameSetting/NickNameSettingPage.tsx
+++ src/pages/account/signUp/nickNameSetting/NickNameSettingPage.tsx
@@ -1,15 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import S from './NickNameSettingPage.styled';
 import { SIGN_UP_DATA } from '../SignUpData';
+import { useNavigate } from 'react-router-dom';
+import { useNaverSignUp } from '../../../../hooks';
+import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+import { naverSignUpState, userState } from '../../../../atom';
 
 const NickNameSettingPage = () => {
+	const navigate = useNavigate();
+
 	const title = `밋팀에서 사용하실\n닉네임을 설정해주세요`;
 
-	const [nickName, setNickName] = useState<string>('');
+	const [signUp, setSignUp] = useRecoilState(naverSignUpState);
+
+	const setUserState = useSetRecoilState(userState);
+	const naverSignUp = useRecoilValue(naverSignUpState);
 
 	const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const newNickName = e.target.value;
-		setNickName(() => newNickName);
+		const { name, value } = e.target;
+		setSignUp({ ...signUp, [name]: value });
+	};
+
+	const checkNaverSignUpInSuccess = () => {
+		navigate('/');
+		localStorage.removeItem('naverSignUpState');
+	};
+
+	const { mutate } = useNaverSignUp({ onSuccess: checkNaverSignUpInSuccess, setUserState });
+
+	const naverSignUpHandler = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(naverSignUp);
+		console.log(userState);
+		naverSignUp && mutate(naverSignUp);
 	};
 
 	return (
@@ -17,7 +40,7 @@ const NickNameSettingPage = () => {
 			<header className='account__header'>
 				<h1>{title}</h1>
 			</header>
-			<S.NickNameSettingPageForm>
+			<S.NickNameSettingPageForm onSubmit={e => naverSignUpHandler(e)}>
 				{SIGN_UP_DATA.map(
 					({ type, placeholder, name }, index) =>
 						name === 'nickName' && (
@@ -27,13 +50,15 @@ const NickNameSettingPage = () => {
 									type={type}
 									placeholder={placeholder}
 									name={name}
-									value={nickName}
+									value={signUp?.nickName}
 									onChange={e => changeHandler(e)}
 								/>
 							</label>
 						)
 				)}
-				<S.NickNameSettingButton type='submit'>확인</S.NickNameSettingButton>
+				<S.NickNameSettingButton type='submit' value='signUp'>
+					확인
+				</S.NickNameSettingButton>
 			</S.NickNameSettingPageForm>
 		</S.NickNameSettingPageLayout>
 	);
