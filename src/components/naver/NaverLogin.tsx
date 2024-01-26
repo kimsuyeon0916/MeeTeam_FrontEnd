48c980dae0eef3814efd61e6e2cb917e2e11c060
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-26 17:30:09 +0900

    feat: #44 네이버 버튼 클릭 시 fetch 적용
    - 이미 연동된 회원인 경우: 메인 페이지로 리다이렉션
    - 신규 유저인 경우: 학사 정보 입력 페이지로 리다이렉션

--- src/components/naver/NaverLogin.tsx
+++ src/components/naver/NaverLogin.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import S from './NaverLogin.styled';
+import { useNavigate } from 'react-router-dom';
+import { useCheckExist } from '../../hooks';
 
 const NaverLogin = () => {
-	const initializeNaverLogin = () => {
+	const navigate = useNavigate();
+
+	const initializeNaver = () => {
 		const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
 			import.meta.env.VITE_NAVER_CLIENT_ID
 		}&state=${import.meta.env.VITE_NAVER_STATE}&redirect_uri=${
@@ -17,13 +21,19 @@ const NaverLogin = () => {
 		return urlParams.get('code');
 	};
 
+	const handleNaverSignInSuccess = () => {
+		data?.token ? navigate('/') : navigate('/signUp/school');
+	};
+
+	const { data, mutate } = useCheckExist({ onSuccess: handleNaverSignInSuccess });
+
 	useEffect(() => {
 		const code = getAuthCode();
-	}, []);
-
+		code && mutate({ code: code });
+	}, [mutate]);
 
 	return (
-		<S.NaverCustomButton onClick={initializeNaverLogin}>
+		<S.NaverCustomButton onClick={initializeNaver}>
 			<img src='/src/assets/NaverIcon.png' className='sign-in__naver-icon' alt='naverIcon' />
 			<span>네이버 로그인</span>
 		</S.NaverCustomButton>
