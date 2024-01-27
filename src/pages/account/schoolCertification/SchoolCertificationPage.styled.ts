31eb4a936911c41237b1ef858cc2db116688333f
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-01-27 10:57:39 +0900

    style: #44 인증 이메일 전송 후 마커 스타일링

--- src/pages/account/schoolCertification/SchoolCertificationPage.styled.ts
+++ src/pages/account/schoolCertification/SchoolCertificationPage.styled.ts
@@ -7,13 +7,34 @@ const SchoolCertificationLayout = styled(Account.AccountLayout)`
 	}
 `;
 
-const SchoolCertificationPageForm = styled(Account.AccountForm)``;
+const SchoolCertificationPageForm = styled(Account.AccountForm)<{ $submitEmail?: boolean }>`
+	align-items: center;
+	justify-content: center;
+	${props => props.$submitEmail && 'row-gap: 3.35rem; width: auto;'}
+`;
 
 const SchoolCertificationButton = styled(Account.AccountButton)`
 	width: 16.5rem;
 	margin: 0 auto;
 `;
 
-const S = { SchoolCertificationLayout, SchoolCertificationPageForm, SchoolCertificationButton };
+const SchoolCertificationMark = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 1.06rem 2.25rem;
+	border-radius: 0.6rem;
+	color: var(--text-color, #151515);
+	background: var(--sub2-color, #f3f5ff);
+	font-size: 1.2rem;
+	cursor: pointer;
+`;
+
+const S = {
+	SchoolCertificationLayout,
+	SchoolCertificationPageForm,
+	SchoolCertificationButton,
+	SchoolCertificationMark,
+};
 
 export default S;
