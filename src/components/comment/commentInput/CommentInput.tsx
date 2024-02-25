914a19ecb8eecb04a2a47370430dd581076162ad
prgmr99 <johnyeom24@naver.com>
2024-02-25 15:03:38 +0900

    chore: form 태그 추가

--- src/components/comment/commentInput/CommentInput.tsx
+++ src/components/comment/commentInput/CommentInput.tsx
@@ -12,26 +12,32 @@ const CommentInput = ({
 }: CommentInputFunctions) => {
 	const isLogin = true; // 임시 코드
 
+	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+	};
+
 	return (
 		<S.CommentInput>
-			<article className='user-input'>
-				<section className='user-input__icon'>
-					<Icon />
-				</section>
-				<section className='user-input__container'>
-					<input
-						type='text'
-						onKeyPress={onKeyPress}
-						value={contents}
-						onChange={onChangeHandler}
-						onClick={onClickInput}
-						placeholder={isLogin ? '댓글 추가...' : '로그인이 필요합니다.'}
-					/>
-					<button type='button' onClick={addComment}>
-						댓글
-					</button>
-				</section>
-			</article>
+			<form onSubmit={onSubmit}>
+				<article className='user-input'>
+					<section className='user-input__icon'>
+						<Icon />
+					</section>
+					<section className='user-input__container'>
+						<input
+							type='text'
+							onKeyPress={onKeyPress}
+							value={contents}
+							onChange={onChangeHandler}
+							onClick={onClickInput}
+							placeholder={isLogin ? '댓글 추가...' : '로그인이 필요합니다.'}
+						/>
+						<button type='submit' onClick={addComment}>
+							댓글
+						</button>
+					</section>
+				</article>
+			</form>
 		</S.CommentInput>
 	);
 };
