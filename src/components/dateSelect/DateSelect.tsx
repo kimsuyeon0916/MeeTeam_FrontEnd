0ec6a04badeaf46967f23f4033cdf3632f43b29a
prgmr99 <johnyeom24@naver.com>
2024-02-28 15:15:01 +0900

    feat: 기간 전역 상태 관리

--- src/components/dateSelect/DateSelect.tsx
+++ src/components/dateSelect/DateSelect.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
-import DatePicker from 'react-datepicker';
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import S from './DateSelect.styled';
 import 'react-datepicker/dist/react-datepicker.css';
+import { recruitInputState } from '../../atom';
+import { simpleDate } from '../../utils';
 
 const DateSelect = () => {
 	const [date, setDate] = useState([new Date(), new Date()]);
 	const [startDate, endDate] = date;
+	const [info, setInfo] = useRecoilState(recruitInputState);
+
+	const onChangeHandler = (update: any) => {
+		setDate(update);
+		const startDate = simpleDate(update[0]);
+		const endDate = simpleDate(update[1]);
+		setInfo({ ...info, period: [startDate, endDate] });
+	};
 
 	return (
 		<S.DateSelect>
@@ -16,9 +26,7 @@ const DateSelect = () => {
 				dateFormat='yyyy년 MM월 dd일'
 				showPopperArrow={false}
 				endDate={endDate}
-				onChange={update => {
-					setDate(update as any);
-				}}
+				onChange={update => onChangeHandler(update)}
 				isClearable={true}
 				className='date-picker'
 			/>
