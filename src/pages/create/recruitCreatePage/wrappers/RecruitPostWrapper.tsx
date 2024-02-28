5f1fbd25d22546f1c5382f89aa0de729e14d35d1
prgmr99 <johnyeom24@naver.com>
2024-02-28 16:19:13 +0900

    feat: 구인글 작성 - 제목, 포스팅 내용 전역 관리

--- src/pages/create/recruitCreatePage/wrappers/RecruitPostWrapper.tsx
+++ src/pages/create/recruitCreatePage/wrappers/RecruitPostWrapper.tsx
@@ -1,24 +1,48 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { modules } from '../../../../utils/index';
 import { Subtitle } from '../../../../components';
 import S from './RecruitPostWrapper.styled';
+import { useRecoilState } from 'recoil';
+import { recruitInputState } from '../../../../atom';
 
 const RecruitPostWrapper = () => {
 	const quillRef = useRef<ReactQuill | null>(null);
+	const [info, setInfo] = useRecoilState(recruitInputState);
+	const [posting, setPosting] = useState({
+		title: '',
+		contents: '',
+	});
+
+	const onChangeContents = (contents: string) => {
+		setPosting({ ...posting, contents: contents });
+		setInfo({ ...info, contents: contents });
+	};
+
+	const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const titleValue = event.target.value;
+		setPosting({ ...posting, title: titleValue });
+		setInfo({ ...info, title: titleValue });
+	};
 
 	return (
 		<S.RecruitPostWrapper>
 			<article className='title'>
 				<Subtitle>{'제목'}</Subtitle>
-				<input type='text' placeholder='제목을 입력해주세요' />
+				<input type='text' placeholder='제목을 입력해주세요' onChange={onChangeTitle} />
 			</article>
 			<article className='container__intro'>
 				<Subtitle>{'구인글'}</Subtitle>
-				<div>
-					<ReactQuill className='editor' ref={quillRef} theme='snow' modules={modules} />
-				</div>
+				<section>
+					<ReactQuill
+						className='editor'
+						ref={quillRef}
+						theme='snow'
+						modules={modules}
+						onChange={onChangeContents}
+					/>
+				</section>
 			</article>
 		</S.RecruitPostWrapper>
 	);
