44363d9f02ae1807a15a0776af9c3cd929525c66
prgmr99 <johnyeom24@naver.com>
2024-02-28 14:29:54 +0900

    chore: #63 원상 복구

--- src/components/optionMenu/recruitOptions/RecruitOptions.styled.ts
+++ src/components/optionMenu/recruitOptions/RecruitOptions.styled.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-interface OptionSelected {
-	$isClicked: boolean;
-}
-
 const RecruitOptions = styled.article`
 	margin-top: 1.2rem;
 	display: flex;
@@ -23,22 +19,13 @@ const RecruitOptions = styled.article`
 		padding: 1.2rem 2.4rem;
 		cursor: pointer;
 	}
+
+	.highlighted {
+		background-color: #e0e6ff;
+		color: #000;
+	}
 `;
 
-const Option = styled.span<OptionSelected>`
-	border-radius: 1rem;
-	border: 1px solid ${props => (props.$isClicked ? '#e0e6ff' : '#e3e3e3')};
-	background: ${props => (props.$isClicked ? '#e0e6ff' : '#fff')};
-	color: ${props => (props.$isClicked ? '#000' : '#8e8e8e')};
-	font-size: 1.5rem;
-	font-style: normal;
-	font-weight: 500;
-	line-height: normal;
-	letter-spacing: 0.02rem;
-	padding: 1.2rem 2.4rem;
-	cursor: pointer;
-`;
-
-const S = { RecruitOptions, Option };
+const S = { RecruitOptions };
 
 export default S;
--- src/components/optionMenu/recruitOptions/RecruitOptions.tsx
+++ src/components/optionMenu/recruitOptions/RecruitOptions.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { OptionList } from '../../../types';
 import S from './RecruitOptions.styled';
-import Option from './Option';
+import { useRecoilState } from 'recoil';
+import { recruitInputState } from '../../../atom';
 
 const RecruitOptions = ({ options, isScope, isCategory }: OptionList) => {
+	const optionsArr = Array(options.length).fill(false);
+	const [isClicked, setIsClicked] = useState<boolean[]>(optionsArr);
+	const [info, setInfos] = useRecoilState(recruitInputState);
+
+	const onClickHandler = (event: React.MouseEvent<HTMLSpanElement>) => {
+		const index = Number(event.currentTarget.id);
+		const newArr = optionsArr.map((_, i) => i === index);
+		setIsClicked(newArr);
+
+		const { innerText } = event.target as HTMLElement;
+		if (isScope) {
+			setInfos({ ...info, scope: innerText });
+		} else if (isCategory) {
+			setInfos({ ...info, category: innerText });
+		}
+		// console.log(isClicked);
+	};
+
 	return (
 		<S.RecruitOptions>
 			{options.map((option, index) => (
-				<Option isScope={isScope} isCategory={isCategory} key={index}>
+				<span className='option' key={index} onClick={onClickHandler} id={index.toString()}>
 					{option}
-				</Option>
+				</span>
 			))}
 		</S.RecruitOptions>
 	);
