3df77a72d345428387a06bb95ec5ee6c35da0c97
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-08 15:58:54 +0900

    feat: #70 유저 관련 UpdateProfilePayload 타입 추가

--- src/types/payload.ts
+++ src/types/payload.ts
@@ -1,7 +1,30 @@
+import { Contact, Award, Link } from './index';
+
 export interface SignUpPayload {
 	school?: string;
 	major?: string;
 	year?: string;
 	email?: string;
 	nickName?: string;
+}
+
+export interface UpdateProfilePayload {
+	imageUrl?: string;
+	nickname: string;
+	userName?: string;
+	isUserNamePublic: boolean;
+	interest?: string;
+	introduction?: string;
+	aboutMe?: string;
+	email: Contact[]; // 논의 필요
+	phone?: Contact;
+	university: string;
+	department: string;
+	maxGpa?: string;
+	gpa?: string;
+	year: string;
+	skills?: string[];
+	awards?: Award[];
+	links?: Link[];
+	portfolios: string[];
 }
