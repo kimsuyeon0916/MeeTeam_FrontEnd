a3b4d9b2fd53d83c0ca90f2b1809812d2c924d09
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-08 15:52:49 +0900

    feat: #70 유저 관련 User, Contact, Skill, Award, Link, Portfolio 타입 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,19 @@
 import type { SignUpPayload } from './payload';
-import type { UserReponse, User } from './response';
 import type { CustomInstance } from './api';
 import type { Comment, CommentInputFunctions } from './comment';
 import type { JsxElementComponentProps } from './indexSigniture';
+import type { User, Contact, Skill, Award, Link, Portfolio } from './user';
 
 export type {
 	SignUpPayload,
-	UserReponse,
 	User,
 	CustomInstance,
 	Comment,
 	JsxElementComponentProps,
 	CommentInputFunctions,
+	Contact,
+	Skill,
+	Award,
+	Link,
+	Portfolio,
 };
