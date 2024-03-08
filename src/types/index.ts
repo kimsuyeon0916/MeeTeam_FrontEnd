c1da6795816754ec252db2667c86194a707091b9
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-08 16:02:26 +0900

    feat: #70 유저 관련 UserResponse 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-import type { SignUpPayload } from './payload';
+import type { SignUpPayload, UpdateProfilePayload } from './payload';
+import type { UserReponse } from './response';
 import type { CustomInstance } from './api';
 import type { Comment, CommentInputFunctions } from './comment';
 import type { JsxElementComponentProps } from './indexSigniture';
@@ -6,11 +7,13 @@ import type { User, Contact, Skill, Award, Link, Portfolio } from './user';
 
 export type {
 	SignUpPayload,
+	UserReponse,
 	User,
 	CustomInstance,
 	Comment,
 	JsxElementComponentProps,
 	CommentInputFunctions,
+	UpdateProfilePayload,
 	Contact,
 	Skill,
 	Award,
