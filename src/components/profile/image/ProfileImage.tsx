c1c8e7c58cc6200abd358762df1e63a8e0ec1da4
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-10 20:47:53 +0900

    feat: #70 프로필 이미지 클릭하면 프로필 페이지로 이동

--- src/components/profile/image/ProfileImage.tsx
+++ src/components/profile/image/ProfileImage.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import S from './ProfileImage.styled';
 import { DefaultProfileImage } from '../../../assets';
+import { useNavigate } from 'react-router';
 
 interface ProfileImage {
+	nickname: string;
 	size: string;
 	url?: string;
 }
 
-const ProfileImage = ({ size, url }: ProfileImage) => {
+const ProfileImage = ({ nickname, size, url }: ProfileImage) => {
+	const navigate = useNavigate();
+
 	return (
-		<S.ProfileImageLayout $size={size} $url={url}>
+		<S.ProfileImageLayout $size={size} $url={url} onClick={() => navigate(`/profile/${nickname}`)}>
 			<S.ProfileImage src={url ? url : DefaultProfileImage} alt='프로필이미지' />
 		</S.ProfileImageLayout>
 	);
