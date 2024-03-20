e125c1fa09812af478de6e695e6b9b696ecdc0a1
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-21 02:28:26 +0900

    feat: #55 콤보 박스 공통 컴포넌트 props 및 로직 변경
    - 이유: 내부가 아닌 외부에서 useForm의 리턴값들(구조분해할당이용)을 주입시켜주는 방식을 적용

--- src/components/comboBox/ComboBox.tsx
+++ src/components/comboBox/ComboBox.tsx
@@ -1,25 +1,40 @@
 import React, { useState, useRef, useEffect, useImperativeHandle } from 'react';
 import S from './comboBox.styled';
 import { OptionList } from '../index';
-import { useForm, FieldValues } from 'react-hook-form';
-import { DevTool } from '@hookform/devtools';
+import { FieldValues, UseFormRegister, UseFormSetValue, FormState, Path } from 'react-hook-form';
 
 interface Option {
 	id: string;
 	title: string;
 }
 
-interface ComboBox {
-	name: string;
+interface ComboBox<T extends FieldValues> {
+	register: UseFormRegister<T>;
+	setValue: UseFormSetValue<T> | ((T, ...U) => UseFormSetValue<T>);
+	formState?: FormState<T>;
+	name: Path<T>;
 	label?: string;
 	icon?: JSX.Element;
 	placeholder?: string;
-	optionList: Option[];
-	isOpen?: boolean;
+	optionList?: Option[];
+	clickInput?: (T) => void;
+	clickOption?: (T) => void;
 }
 
-const ComboBox = ({ name, label, icon, optionList, ...props }: ComboBox) => {
-	const { register, setValue, control } = useForm<FieldValues>();
+const ComboBox = <T extends FieldValues>({
+	register,
+	setValue,
+	formState,
+	name,
+	label,
+	icon,
+	optionList,
+	clickInput,
+	clickOption,
+	...props
+}: ComboBox<T>) => {
+	const inputErrorMessage = formState?.errors[name]?.message;
+
 	const { ref, ...rest } = register(name);
 
 	const [isOpen, setIsOpen] = useState(false);
@@ -40,12 +55,14 @@ const ComboBox = ({ name, label, icon, optionList, ...props }: ComboBox) => {
 	}, [isOpen]);
 
 	const handleInputClick = () => {
+		clickInput?.(name);
 		setIsOpen(true);
 	};
 
-	const handleOptionClick = (name: string, id: string, title: string) => {
+	const handleOptionClick = (name: string, title: string, id?: string) => {
 		setValue(name, title);
-		// 해당 title의 id를 payload로 설정
+		id && sessionStorage.setItem(name, id);
+		clickOption?.(name);
 	};
 
 	return (
@@ -55,10 +72,9 @@ const ComboBox = ({ name, label, icon, optionList, ...props }: ComboBox) => {
 				<S.ComboBoxInput {...rest} {...props} ref={dropdownRef} onClick={handleInputClick} />
 				{icon}
 			</S.ComboBoxLabel>
-			{isOpen && (
+			{optionList && isOpen && (
 				<OptionList name={name} optionList={optionList} handleOptionClick={handleOptionClick} />
 			)}
-			<DevTool control={control} />
 		</S.ComboBoxLayout>
 	);
 };
