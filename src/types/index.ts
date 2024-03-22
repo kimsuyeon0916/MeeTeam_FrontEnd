3258559507b906b67d605ce089fe00bebfa5c3a5
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-22 09:44:07 +0900

    feat: #55 유저 관련 User, Contact, Skill, Award, Link, Portfolio 타입 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,10 @@
 import type { SignUpPayload } from './payload';
-import type { UserReponse, User, University, Department } from './response';
+import type { UserReponse, University, Department } from './response';
 import type { CustomInstance } from './api';
 import type { TitleInfo, OptionList, Option, Role, InputRoleForm, InputState } from './information';
 import type { Comment, CommentInputFunctions } from './comment';
 import type { JsxElementComponentProps } from './indexSigniture';
+import type { User, Contact, Skill, Award, Link, Portfolio } from './user';
 
 export type {
 	SignUpPayload,
@@ -13,6 +14,11 @@ export type {
 	Comment,
 	JsxElementComponentProps,
 	CommentInputFunctions,
+	Contact,
+	Skill,
+	Award,
+	Link,
+	Portfolio,
 	TitleInfo,
 	OptionList,
 	Option,
