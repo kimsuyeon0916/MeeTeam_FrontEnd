6491ab01853340242bad50f5c4e1ab040490bbe4
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-22 09:43:44 +0900

    feat: #55 유저 관련 UserResponse 타입 변경

--- src/types/response.ts
+++ src/types/response.ts
@@ -1,45 +1,11 @@
 export interface UserReponse {
-	user: User;
-	accessToken: string;
+	userName?: string; // userName -> nickname 으로 데이터 변경 요청 필요
+	pictureUrl?: string; // imageUrl로 변경 요청 필요
+	accessToken?: string;
 	refreshToken?: string;
 	platformId?: string;
 	isEnable?: boolean;
 }
-
-export interface User {
-	university: string;
-	department: string;
-	year: string;
-	email: string;
-	nickname: string;
-	username?: string;
-	imageUrl?: string;
-	rate?: number;
-	phone?: '';
-	position?: string;
-	introduction?: string;
-	specifications?: string[];
-	links?: string[];
-	meeteam?: Meeteam[];
-	comment?: Comment[];
-	followers?: [];
-	following?: [
-		{
-			_id: string;
-			user: string;
-			follower: string;
-			createdAt: string;
-			updatedAt: string;
-		},
-	];
-	timeTable?: TimeTable[];
-}
-
-interface Meeteam {}
-
-interface Comment {}
-
-interface TimeTable {}
 
 export interface University {
 	universityId: string;
