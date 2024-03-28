63b39b6f4b3159c4f83a77b45e15053961677ca8
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-03-28 17:29:26 +0900

    feat: #71 유저 프로필 조회 API 호출 커스텀 hook 변경
    - 이유: 유저 프로필 조회 API 명세 변경 반영

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { readProfile } from '../service';
 
 const profileKeys = {
-	readProfile: (nickname: string) => ['readProfile', nickname],
+	readProfile: (userId: string) => ['readProfile', userId],
 };
 
 /**
  * @description 유저 프로필 조회 API를 호출하는 hook입니다.
  */
-export const useReadProfile = (nickname: string) => {
+export const useReadProfile = (userId: string) => {
 	return useQuery({
-		queryKey: profileKeys.readProfile(nickname),
-		queryFn: () => readProfile(nickname),
+		queryKey: profileKeys.readProfile(userId),
+		queryFn: () => readProfile(userId),
 	});
 };
