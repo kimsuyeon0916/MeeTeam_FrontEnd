e6a446b776de922865e2038ea96a8eb6d5721c6a
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-04 03:48:52 +0900

    style: #71 기본/주요 버튼 공통 컴포넌트 스타일링 추가

--- src/components/button/Button.styled.ts
+++ src/components/button/Button.styled.ts
@@ -16,6 +16,45 @@ const IconButtonLayout = styled.button<{ $add?: boolean }>`
 			: `border: 1px solid #8E8E8E; background: #F6F6F6;`}
 `;
 
-const S = { IconButtonLayout };
+const DefaultButtonLayout = styled.button<{ $small?: boolean }>`
+	all: unset;
+	box-sizing: border-box;
+	display: flex;
+	height: ${props => (props.$small ? '3.6rem' : '4.8rem')};
+	padding: ${props => (props.$small ? '1.2rem 2rem' : '1.2rem 3.2rem')};
+	justify-content: center;
+	align-items: center;
+	border: 1px solid var(--ButtonColors-Default-outline-defaultLine, #e3e3e3);
+	border-radius: 0.6rem;
+	color: var(--text-color-2, #373f41);
+	margin-left: auto;
+	cursor: pointer;
+
+	${props =>
+		props.$small
+			? `
+					/* ButtonTxt/small */
+					font-size: 1.4rem;
+					font-style: normal;
+					font-weight: 500;
+					line-height: 1.7rem; /* 121.429% */
+					letter-spacing: 0.0028rem;
+				`
+			: `
+					/* ButtonTxt/big */
+					font-size: 1.6rem;
+					font-style: normal;
+					font-weight: 500;
+					line-height: 1.9rem; /* 118.75% */
+					letter-spacing: 0.0032rem;
+				`}
+`;
+const PrimaryButtonLayout = styled(DefaultButtonLayout)`
+	border: 0;
+	background: var(--main-color, #5877fc);
+	color: var(--ButtonColors-Primary-outline-default, #fff);
+`;
+
+const S = { IconButtonLayout, DefaultButtonLayout, PrimaryButtonLayout };
 
 export default S;
