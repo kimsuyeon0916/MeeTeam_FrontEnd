70bce30c0d3a2ec89e753c508def1fff67fdd6c2
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-04 03:57:49 +0900

    style: #71 포트폴리오 카드 공통 컴포넌트 스타일링 변경 및 추가
    - 포트폴리오 비율 변경
    - 포트폴리오 카드 내 숫자 버튼 스타일링 추가

--- src/components/portfolio/card/PortfolioCard.styled.ts
+++ src/components/portfolio/card/PortfolioCard.styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const PortfolioCardLayout = styled.article`
 	display: flex;
 	flex-direction: column;
+	row-gap: 0.8rem;
 
 	cursor: pointer;
 `;
@@ -13,7 +14,7 @@ const PortfolioCardBox = styled.div<{ $url?: string }>`
 	border-radius: 0.75rem;
 	overflow: hidden;
 
-	aspect-ratio: 183 / 114;
+	aspect-ratio: 183 / 103; // 포트폴리오 비율
 `;
 
 const PortfolioCardImage = styled.img`
@@ -64,6 +65,29 @@ const PortfolioCardTag = styled.span`
 	letter-spacing: 0.02rem;
 `;
 
+const PortfolioCardButton = styled.button<{ $checked?: boolean }>`
+	position: absolute;
+	bottom: 1rem;
+	right: 1rem;
+
+	box-sizing: border-box;
+	display: flex;
+	width: 2.8rem;
+	height: 2.8rem;
+	justify-content: center;
+	align-items: center;
+	border-radius: 10rem;
+	${props => !props.$checked && 'border: 1px solid #fff'};
+	background: ${props =>
+		props.$checked ? 'var(--main-color, #5877FC)' : 'rgba(255, 255, 255, 0.3)'};
+	color: var(--ButtonColors-Primary-outline-default, #fff);
+
+	font-size: 1.6rem;
+	font-weight: 600;
+	line-height: 2.4rem;
+	letter-spacing: 0.0032rem;
+`;
+
 const S = {
 	PortfolioCardLayout,
 	PortfolioCardBox,
@@ -71,6 +95,7 @@ const S = {
 	PortfolioCardTitle,
 	PortfolioTagRow,
 	PortfolioCardTag,
+	PortfolioCardButton,
 };
 
 export default S;
