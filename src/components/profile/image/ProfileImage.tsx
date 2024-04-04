cea97292806bb9477d491b40731f6936b9e0cce8
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-04 13:02:48 +0900

    feat: #71 프로필 이미지 공통 컴포넌트에  이미지 업로드 기능 추가

--- src/components/profile/image/ProfileImage.tsx
+++ src/components/profile/image/ProfileImage.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import S from './ProfileImage.styled';
-import { DefaultProfileImage } from '../../../assets';
+import { AddProfile, DefaultProfileImage } from '../../../assets';
 import { useNavigate } from 'react-router';
 
 interface ProfileImage {
+	isEditable?: boolean;
 	nickname: string;
 	size: string;
 	url?: string;
 }
 
-const ProfileImage = ({ nickname, size, url }: ProfileImage) => {
+const ProfileImage = ({ isEditable, nickname, size, url }: ProfileImage) => {
 	const navigate = useNavigate();
+	const navigateProfile = () => {
+		navigate(`/profile/${nickname}`);
+	};
+
+	const inputRef = useRef<HTMLInputElement>(null);
+	const addImage = () => {
+		inputRef.current?.click();
+	};
+
+	const [imageSrc, setImageSrc] = useState<string | null>(url ? url : null);
+	const changeImage = event => {
+		const uploadImage = event.target?.files[0];
+		const reader = new FileReader();
+		reader.readAsDataURL(uploadImage);
+		reader.onload = () => setImageSrc(reader.result as string);
+	};
 
 	return (
-		<S.ProfileImageLayout $size={size} $url={url} onClick={() => navigate(`/profile/${nickname}`)}>
-			<S.ProfileImage src={url ? url : DefaultProfileImage} alt='프로필이미지' />
+		<S.ProfileImageLayout>
+			<S.ProfileImageWrapper
+				$size={size}
+				$url={imageSrc ? true : false}
+				onClick={isEditable ? addImage : navigateProfile}
+			>
+				<S.ProfileImage src={imageSrc ? imageSrc : DefaultProfileImage} alt='프로필이미지' />
+			</S.ProfileImageWrapper>
+			{isEditable && (
+				<>
+					<S.ProfileAddIcon onClick={addImage} src={AddProfile} alt='프로필이미지 추가' />
+					<S.ProfileImageInput ref={inputRef} type='file' accept='image/*' onChange={changeImage} />
+				</>
+			)}
 		</S.ProfileImageLayout>
 	);
 };
