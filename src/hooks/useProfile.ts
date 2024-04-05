06f7fd2a4174144523a12a700b94278b5a01f950
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-06 00:39:31 +0900

    feat: #71 유저 프로필 작성 API 호출 커스텀 훅 파라미터 변경
    - 구조 분해 할당 적용

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -18,7 +18,7 @@ export const useReadProfile = (userId: string) => {
 /**
  * @description 유저 프로필 작성 API를 호출하는 hook입니다. 기존 회원인 경우 access token 을 로컬 스토리지에 저장합니다. 회원이 아닌 경우, 회원가입 페이지로 이동합니다.
  */
-export const useUpdateProfile = (onSuccess: () => void) => {
+export const useUpdateProfile = ({ onSuccess }: { onSuccess: () => void }) => {
 	return useMutation({
 		mutationFn: updateProfile,
 		onSuccess: () => {
