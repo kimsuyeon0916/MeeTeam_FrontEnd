864e1f703567612994ad1d4359c04e13edbf2e37
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-10 20:52:03 +0900

    feat: #92 포트폴리오 상세 페이지 API fetch 적용 (포트폴리오 다중 이미지 제외)

--- src/pages/portfolio/details/PortfolioDetailsPage.tsx
+++ src/pages/portfolio/details/PortfolioDetailsPage.tsx
@@ -7,61 +7,64 @@ import {
 	PortfolioInformation,
 	PortfolioList,
 } from '../../../components';
-import { portfolioData } from '../portfolioData';
 import { useNavigate, useParams } from 'react-router-dom';
 import { imageList } from '../../../components/carousel/imageList';
+import { useReadPortfolio } from '../../../hooks';
 
 const PortfolioDetailsPage = () => {
 	const { portfolioId } = useParams() as { portfolioId: string };
+	const { data: portfolio, isSuccess } = useReadPortfolio(portfolioId);
 
 	const navigate = useNavigate();
 
 	return (
-		<S.PortfolioDetailsLayout>
-			<S.PortfolioDetailsContainer>
-				<S.PortfolioDetailsHeader>
-					<S.PortfolioDetailsColumn $gap='1rem'>
-						<h1>{portfolioData.title}</h1>
-						<h5>{portfolioData.description}</h5>
-					</S.PortfolioDetailsColumn>
-					{portfolioData.isWriter && (
-						<DefaultBtn
-							type='button'
-							title='편집'
-							handleClick={() => navigate(`/portfolio/${portfolioId}/edit`)}
-						/>
-					)}
-				</S.PortfolioDetailsHeader>
+		isSuccess && (
+			<S.PortfolioDetailsLayout>
+				<S.PortfolioDetailsContainer>
+					<S.PortfolioDetailsHeader>
+						<S.PortfolioDetailsColumn $gap='1rem'>
+							<h1>{portfolio?.title}</h1>
+							<h5>{portfolio?.description}</h5>
+						</S.PortfolioDetailsColumn>
+						{portfolio?.isWriter && (
+							<DefaultBtn
+								type='button'
+								title='편집'
+								handleClick={() => navigate(`/portfolio/${portfolioId}/edit`)}
+							/>
+						)}
+					</S.PortfolioDetailsHeader>
 
-				<S.PortfolioDetailsColumn $gap='8rem'>
-					<S.PortfolioDetailsColumn $gap='4rem'>
-						<ImageCarousel images={imageList} />
-						<PortfolioInformation {...portfolioData} />
-					</S.PortfolioDetailsColumn>
+					<S.PortfolioDetailsColumn $gap='8rem'>
+						<S.PortfolioDetailsColumn $gap='4rem'>
+							<ImageCarousel images={imageList} />
+							<PortfolioInformation {...portfolio} />
+						</S.PortfolioDetailsColumn>
 
-					<S.PortfolioDetailsColumn>
-						<S.PortfolioDetailsArticle>
-							<S.PortfolioDetailsTitle>상세내용</S.PortfolioDetailsTitle>
-							<hr />
-							<S.PortfolioDetailsContent>{portfolioData.content}</S.PortfolioDetailsContent>
-						</S.PortfolioDetailsArticle>
+						<S.PortfolioDetailsColumn>
+							<S.PortfolioDetailsArticle>
+								<S.PortfolioDetailsTitle>상세내용</S.PortfolioDetailsTitle>
+								<hr />
+								<S.PortfolioDetailsContent>{portfolio?.content}</S.PortfolioDetailsContent>
+							</S.PortfolioDetailsArticle>
 
-						<S.PortfolioDetailsArticle>
-							<S.PortfolioDetailsTitle>링크</S.PortfolioDetailsTitle>
-							<hr />
-							<S.PortfolioDetailsColumn>
-								{portfolioData?.links?.map((link, index) => (
-									<S.PortfolioDetailsRow key={index} $gap='3.65rem'>
-										<LinkDetails {...link} />
-									</S.PortfolioDetailsRow>
-								))}
-							</S.PortfolioDetailsColumn>
-						</S.PortfolioDetailsArticle>
+							<S.PortfolioDetailsArticle>
+								<S.PortfolioDetailsTitle>링크</S.PortfolioDetailsTitle>
+								<hr />
+								<S.PortfolioDetailsColumn>
+									{portfolio?.links?.map((link, index) => (
+										<S.PortfolioDetailsRow key={index} $gap='3.65rem'>
+											<LinkDetails {...link} />
+										</S.PortfolioDetailsRow>
+									))}
+								</S.PortfolioDetailsColumn>
+							</S.PortfolioDetailsArticle>
+						</S.PortfolioDetailsColumn>
 					</S.PortfolioDetailsColumn>
-				</S.PortfolioDetailsColumn>
-			</S.PortfolioDetailsContainer>
-			<PortfolioList portfolios={portfolioData.otherPortfolios} />
-		</S.PortfolioDetailsLayout>
+				</S.PortfolioDetailsContainer>
+				<PortfolioList portfolios={portfolio?.otherPortfolios ?? []} />
+			</S.PortfolioDetailsLayout>
+		)
 	);
 };
 
