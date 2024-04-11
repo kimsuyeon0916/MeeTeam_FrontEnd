dcdc3782484194d4e8fbd7ca98fc307e891b8de5
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-11 16:26:22 +0900

    refactor: #71 MuiDatepicker 공통 컴포넌트를 Controller 제외시킨 공통 컴포넌트로 변경

--- src/components/muiDatepicker/MuiDatepicker.tsx
+++ src/components/muiDatepicker/MuiDatepicker.tsx
@@ -1,45 +1,41 @@
 import React from 'react';
-import { Controller, Control } from 'react-hook-form';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import S from './MuiDatepicker.styled';
 import { GrayCalendar } from '../../assets';
 import { format } from 'date-fns';
 
-interface Date {
-	name: string;
-	control: Control;
+interface MuiDatepicker {
+	handleChange: (value: Date | null) => void;
+	defaultValue?: Date;
+	value?: Date;
+	inputRef?: React.Ref<HTMLInputElement>;
 }
 
 const CalendarIcon = () => {
 	return <img src={GrayCalendar} alt='calendar' />;
 };
 
-const MuiDatepicker = ({ name, control }: Date) => {
+const MuiDatepicker = ({ handleChange, defaultValue, value, inputRef }: MuiDatepicker) => {
 	return (
-		<Controller
-			control={control}
-			name={name}
-			render={({ field: { onChange, ref, value } }) => (
-				<DatePicker
-					defaultValue={new Date(value)}
-					slots={{
-						popper: S.StyledPopper,
-						textField: S.StyledTextField,
-						layout: S.StyledPickersLayout as React.JSXElementConstructor<any>,
-						openPickerIcon: CalendarIcon,
-					}}
-					slotProps={{
-						textField: {
-							placeholder: format(new Date(), 'yy년 MM월 dd일'),
-						},
-					}}
-					views={['year', 'month', 'day']}
-					format='yy년 MM월 dd일'
-					disableHighlightToday={true}
-					onChange={onChange}
-					inputRef={ref}
-				/>
-			)}
+		<DatePicker
+			defaultValue={defaultValue}
+			value={value}
+			slots={{
+				popper: S.StyledPopper,
+				textField: S.StyledTextField,
+				layout: S.StyledPickersLayout as React.JSXElementConstructor<any>,
+				openPickerIcon: CalendarIcon,
+			}}
+			slotProps={{
+				textField: {
+					placeholder: format(new Date(), 'yy년 MM월 dd일'),
+				},
+			}}
+			views={['year', 'month', 'day']}
+			format='yy년 MM월 dd일'
+			disableHighlightToday={true}
+			onChange={handleChange}
+			inputRef={inputRef}
 		/>
 	);
 };
