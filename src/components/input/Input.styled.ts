9136770c4f18d83628b2b9f3e282e5c8944a44f9
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-04-16 16:31:51 +0900

    style: #91 입력폼 공통 컴포넌트 내 라벨 스타일링

--- src/components/input/Input.styled.ts
+++ src/components/input/Input.styled.ts
@@ -8,7 +8,7 @@ interface InputStyle {
 	invalid?: boolean;
 }
 
-const InputLabel = styled.label<{ $width?: string }>`
+const InputLayout = styled.label<{ $width?: string }>`
 	min-width: 0;
 	display: flex;
 	flex-direction: column;
@@ -24,8 +24,6 @@ const InputLabel = styled.label<{ $width?: string }>`
 	letter-spacing: 0.0032rem;
 
 	h6 {
-		margin-bottom: 0.8rem;
-
 		/* Body/body2/semibold */
 		font-size: 1.4rem;
 		font-style: normal;
@@ -33,6 +31,17 @@ const InputLabel = styled.label<{ $width?: string }>`
 		line-height: 1.7rem; /* 121.429% */
 		letter-spacing: 0.0028rem;
 	}
+`;
+
+const InputLabel = styled.h6<{ $required?: boolean }>`
+	margin-bottom: 0.8rem;
+
+	${props =>
+		props.$required &&
+		`&:: after {
+			content: ' *';
+			color: #f85858;
+		}`}
 `;
 
 const InputContainer = styled.div`
@@ -106,6 +115,6 @@ const Input = styled.input<InputStyle>`
 	}
 `;
 
-const S = { InputLabel, InputContainer, Input };
+const S = { InputLayout, InputLabel, InputContainer, Input };
 
 export default S;
