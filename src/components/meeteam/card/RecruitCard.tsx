5eb8f78042bf472e8dca21e956d7e68ad4668d4d
prgmr99 <johnyeom24@naver.com>
2024-04-17 23:34:36 +0900

    fix: #99 북마크 버그 수정

--- src/components/meeteam/card/RecruitCard.tsx
+++ src/components/meeteam/card/RecruitCard.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import S from './Card.styled';
 import { useNavigate } from 'react-router-dom';
 import { FilledBookmark, UnfilledBookmark } from '../../../assets';
 import { ProfileImage } from '../..';
 import { Post } from '../../../types';
+import { useBookmark } from '../../../hooks';
+import { useDelBookmark } from '../../../hooks/useBookMark';
+import { useQueryClient } from '@tanstack/react-query';
 
 const RecruitCard = ({
 	id,
@@ -18,7 +21,13 @@ const RecruitCard = ({
 	isClosed,
 }: Post) => {
 	const navigate = useNavigate();
+	const queryClient = useQueryClient();
 	const [isMarked, setIsMarked] = useState<boolean>(isBookmarked);
+	const onSuccess = () => {
+		queryClient.invalidateQueries({ queryKey: ['recruit_board'] });
+	};
+	const { mutate: bookmarked } = useBookmark();
+	const { mutate: unBookmarked } = useDelBookmark();
 
 	const onClickContent = () => {
 		navigate(`/recruitment/postings/${id}`);
@@ -26,8 +35,17 @@ const RecruitCard = ({
 
 	const onClickBookmark = (event: React.MouseEvent<HTMLDivElement>) => {
 		event.stopPropagation();
+		if (!isMarked) {
+			bookmarked(Number(id));
+		} else {
+			unBookmarked(Number(id));
+		}
 		setIsMarked(prev => !prev);
 	};
+
+	useEffect(() => {
+		setIsMarked(isBookmarked);
+	}, [isBookmarked]);
 
 	return (
 		<S.RecruitCard onClick={onClickContent} $isClosed={isClosed}>
--- src/hooks/useBookMark.ts
+++ src/hooks/useBookMark.ts
@@ -1,6 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { bookmarkDelete, bookmarkPost } from '../service/recruit/detail';
 
+interface BookmarkSuccess {
+	onSuccess?: () => void;
+}
+
 export const useBookmark = () => {
 	return useMutation({
 		mutationFn: (id: number) => bookmarkPost(id),
