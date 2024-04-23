cd8a34f8629c89c14f94e8ba3f3459725b46bda3
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-04-24 01:50:05 +0900

    feat: #45 로그인 페이지 변경 로직 적용

--- src/pages/account/signIn/SignInPage.tsx
+++ src/pages/account/signIn/SignInPage.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import S from './SignInPage.styled';
 import { NaverLogin } from '../../../components';
 import { useNavigate } from 'react-router-dom';
-import { EndPoint } from '../../../service';
 import { useCheckExist } from '../../../hooks';
 import { useSetRecoilState } from 'recoil';
 import { userState } from '../../../atom';
+import { MeeteamLogoLarge } from '../../../assets';
 
 const SignInPage = () => {
 	const navigate = useNavigate();
@@ -39,46 +39,9 @@ const SignInPage = () => {
 	return (
 		<S.SignInPageLayout $callBack={callBack}>
 			<header className='sign-in__header'>
-				<h1>로그인</h1>
+				<img src={MeeteamLogoLarge} alt='밋팀로고' />
 			</header>
-			<S.SignInPageForm method='post' action={import.meta.env.VITE_BASE_URL + EndPoint.SIGN_IN}>
-				<label className='sign-in__label'>
-					<input className='account__input' type='text' placeholder='학교 이메일' name='username' />
-				</label>
-				<label className='sign-in__label'>
-					<input
-						className='account__input'
-						type='password'
-						placeholder='비밀번호'
-						name='password'
-					/>
-				</label>
-				<label className='sign-in__label--auto-sign-in'>
-					<input type='checkbox' className='sign-in__input--auto-sign-in' name='autoSignIn' />
-					자동 로그인
-				</label>
-				<S.SignInPageButton type='submit' value='submit'>
-					로그인
-				</S.SignInPageButton>
-			</S.SignInPageForm>
-			<div className='sign-in__button-row'>
-				<button
-					type='button'
-					className='sign-in__button'
-					onClick={() => navigate('/signUp/school')}
-				>
-					회원가입
-				</button>
-				<hr className='sign_in__vertical' />
-				<button
-					type='button'
-					className='sign-in__button'
-					onClick={() => navigate('/find/password')}
-				>
-					비밀번호 찾기
-				</button>
-			</div>
-			<div className='sign-in__social-login-column'>
+			<article>
 				<div className='sign-in__social-login-marker'>
 					<hr className='sign_in__horizon' />
 					소셜 계정 로그인
@@ -87,7 +50,7 @@ const SignInPage = () => {
 				<div>
 					<NaverLogin />
 				</div>
-			</div>
+			</article>
 		</S.SignInPageLayout>
 	);
 };
