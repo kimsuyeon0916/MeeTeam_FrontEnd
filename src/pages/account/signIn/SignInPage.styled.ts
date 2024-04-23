444a33e29413340bdb2570c0eeda2f654895ca5f
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-04-24 01:49:44 +0900

    style: #45 로그인 페이지 변경 UI 스타일링 적용

--- src/pages/account/signIn/SignInPage.styled.ts
+++ src/pages/account/signIn/SignInPage.styled.ts
@@ -9,83 +9,32 @@ const SignInPageLayout = styled(Account.AccountLayout)<{ $callBack?: boolean }>`
 	.sign-in__header {
 		display: flex;
 		line-height: 4.2rem;
-		margin-bottom: 4.49rem;
-		color: var(--text-color-2, #151515);
-		font-size: 2.7rem;
-	}
-
-	.sign-in__button-row {
-		display: flex;
-		flex-direction: row;
-		column-gap: 1rem;
-		margin-right: auto;
-		margin-top: 1.5rem;
-	}
-
-	.sign-in__button {
-		all: unset;
-		font-size: 1.2rem;
-		cursor: pointer;
+		margin: 6rem 0;
 	}
 
 	.sign-in__social-login-marker {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
-		column-gap: 0.68rem;
-		margin-top: 3rem;
+		column-gap: 0.93rem;
+		margin-bottom: 1.2rem;
+		/* Text/t2 */
 		font-size: 1.2rem;
-	}
-
-	.sign-in__social-login-column {
-		display: flex;
-		flex-direction: column;
-		row-gap: 1.5rem;
-	}
-
-	.sign_in__vertical {
-		all: unset;
-		display: flex;
-		width: 0.15rem;
-		height: 1.35rem;
-		background: #d9d9d9;
+		line-height: 1.4rem;
+		letter-spacing: 0.0024rem;
 	}
 
 	.sign_in__horizon {
 		all: unset;
 		display: flex;
-		width: 12.525rem;
+		width: 12.539rem;
 		height: 0.15rem;
 		background: #d9d9d9;
 	}
 `;
 
-const SignInPageForm = styled(Account.AccountForm)`
-	display: flex;
-	flex-direction: column;
-	row-gap: 1.35rem;
-	width: 100%;
-
-	.sign-in__label {
-		display: flex;
-	}
-
-	.sign-in__label--auto-sign-in {
-		display: flex;
-		flex-direction: row;
-		column-gap: 0.6rem;
-		font-size: 1.35rem;
-	}
-
-	.sign-in__input--auto-sign-in {
-		display: flex;
-		margin: 0;
-		cursor: pointer;
-	}
-`;
-
 const SignInPageButton = styled(Account.AccountButton)``;
 
-const S = { SignInPageLayout, SignInPageForm, SignInPageButton };
+const S = { SignInPageLayout, SignInPageButton };
 
 export default S;
