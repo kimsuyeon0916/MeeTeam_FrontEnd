373cb1a02e8055c5c38c235aeb989a50c985bd88
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-04-30 04:17:41 +0900

    style: #94 포트폴리오 관리 페이지 스타일링 추가

---
+++ src/pages/portfolio/management/PortfolioManagementPage.styled.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+const PortfolioManagementLayout = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin: 0 auto;
+	// margin-bottom: 15rem;
+	width: clamp(45%, 96rem, 75%); // width: 96rem;
+	color: var(--Light-Black, #373f41);
+
+	h2 {
+		color: var(--Text-textColor1, #151515);
+		/* Headline/h2 */
+		font-size: 2.4rem;
+		font-weight: 700;
+		line-height: 2.9rem; /* 120.833% */
+		letter-spacing: 0.0048rem;
+	}
+`;
+
+const PortfolioManagementHeader = styled.header`
+	display: flex;
+	flex-direction: row;
+	margin-top: 8rem;
+	margin-bottom: 2rem;
+`;
+
+const PortfolioManagementGrid = styled.div`
+  display: grid;
+	// margin: 8rem 0;
+  margin-top: 8rem;
+	grid-template-columns: repeat(auto-fill, minmax(22.5rem, 1fr));
+	grid-auto-rows: minmax(12.7rem, auto);
+	row-gap: 4rem;
+	column-gap: 2rem;
+
+	/* 스크롤바 숨기기 */
+	overflow-y: auto;
+	-ms-overflow-style: none; /* IE and Edge */
+	scrollbar-width: none; /* Firefox */
+	&::-webkit-scrollbar {
+		display: none; /* Chrome, Safari, Opera*/
+`;
+
+const S = { PortfolioManagementLayout, PortfolioManagementHeader, PortfolioManagementGrid };
+
+export default S;
