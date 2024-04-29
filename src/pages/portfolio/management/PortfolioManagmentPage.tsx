cc85fecef4f9bc8c07644a55627a66f7f97a765f
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-04-30 04:33:28 +0900

    feat: #94 포트폴리오 페이지네이션 조회 fetch

--- src/pages/portfolio/management/PortfolioManagmentPage.tsx
+++ src/pages/portfolio/management/PortfolioManagmentPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import S from './PortfolioManagementPage.styled';
 import { Pagination, PortfolioCard, PrimaryBtn, TabMenu } from '../../../components';
 import { useNavigate } from 'react-router-dom';
-import { portfolioData } from '../portfolioData';
+import { usePaginationPortfolioList } from '../../../hooks';
 
 const tabList = ['전체', '프로젝트', '스터디', '공모전'];
 
@@ -10,31 +10,34 @@ const PortfolioManagementPage = () => {
 	const navigate = useNavigate();
 
 	const [pageNumber, setPageNumber] = useState(1);
+	const { data: portfolioList, isSuccess } = usePaginationPortfolioList(16, pageNumber);
 
 	return (
-		<S.PortfolioManagementLayout>
-			<S.PortfolioManagementHeader>
-				<h2>포트폴리오</h2>
-				<PrimaryBtn
-					type='button'
-					title='포트폴리오 작성'
-					handleClick={() => navigate('/portfolio/edit')}
+		isSuccess && (
+			<S.PortfolioManagementLayout>
+				<S.PortfolioManagementHeader>
+					<h2>포트폴리오</h2>
+					<PrimaryBtn
+						type='button'
+						title='포트폴리오 작성'
+						handleClick={() => navigate('/portfolio/edit')}
+					/>
+				</S.PortfolioManagementHeader>
+				<TabMenu tabList={tabList} />
+				<S.PortfolioManagementGrid>
+					{portfolioList &&
+						portfolioList.portfolios?.map(portfolio => (
+							<PortfolioCard key={portfolio.id} {...portfolio} />
+						))}
+				</S.PortfolioManagementGrid>
+				<Pagination
+					postsNum={portfolioList?.pageInfo.totalContents as number}
+					postsPerPage={portfolioList?.pageInfo.size as number}
+					currentPage={pageNumber}
+					setCurrentPage={setPageNumber}
 				/>
-			</S.PortfolioManagementHeader>
-			<TabMenu tabList={tabList} />
-			<S.PortfolioManagementGrid>
-				{portfolioData &&
-					portfolioData.otherPortfolios?.map(
-						portfolio => portfolio && <PortfolioCard key={portfolio.id} {...portfolio} />
-					)}
-			</S.PortfolioManagementGrid>
-			<Pagination
-				postsNum={60} // 추후 변경
-				postsPerPage={16} // 추후 변경
-				currentPage={pageNumber}
-				setCurrentPage={setPageNumber}
-			/>
-		</S.PortfolioManagementLayout>
+			</S.PortfolioManagementLayout>
+		)
 	);
 };
 
