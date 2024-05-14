304340ab5d37d49781f5f2cfecfcd5759778a1f8
prgmr99 <johnyeom24@naver.com>
2024-05-14 21:11:25 +0900

    style: #180 토스트 UI 수정

--- src/components/recruit/applicants/toast/Toast.styled.ts
+++ src/components/recruit/applicants/toast/Toast.styled.ts
@@ -10,27 +10,28 @@ const ToastContainer = styled.article<ToastProps>`
 	left: 50%;
 	display: flex;
 	align-items: center;
-	width: 48.184rem;
-	padding: 2rem;
+	padding: 2.2rem 4rem;
 	transform: translateX(-50%);
-	background-color: #8358fc;
 	color: #f3f5ff;
-	border-radius: 1rem;
 	gap: 1.2rem;
 	box-sizing: border-box;
-	border: 2px solid #8358fc;
-	box-shadow:
-		0px 4px 20px 0px rgba(0, 0, 0, 0.1),
-		0px 2px 2px 0px rgba(0, 0, 0, 0.25);
 	opacity: 1;
 	visibility: visible;
 	transition: opacity 0.2s ease-in-out;
+	z-index: 10001;
+	border-radius: 5rem;
+	border: 2px solid #6b6f81;
+	background: #6b6f81;
+	box-shadow:
+		0px 4px 20px 0px rgba(0, 0, 0, 0.1),
+		0px 2px 2px 0px rgba(0, 0, 0, 0.25);
 
 	.body1-medium {
 		color: #ffffff;
 		font-size: 1.6rem;
 		letter-spacing: 0.0032rem;
 		margin-top: 0.5rem;
+		font-weight: 600;
 	}
 `;
 
