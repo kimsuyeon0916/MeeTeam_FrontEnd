d4b7f36ca499c67e8650f10e04400b6fe175b655
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-05-17 03:09:33 +0900

    fix: react-hook-form Devtools 배포 브랜치에서 적용 제거

--- src/pages/portfolio/edit/PortfolioEditPage.tsx
+++ src/pages/portfolio/edit/PortfolioEditPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import S from './PortfolioEdit.styled';
-import { DevTool } from '@hookform/devtools';
 import {
 	Input,
 	ComboBox,
@@ -471,7 +470,6 @@ const PortfolioEditPage = () => {
 					</S.PortfolioEditButtonBox>
 				</S.PortfolioEditColumn>
 			</S.PortfolioEditLayout>
-			<DevTool control={control} />
 		</>
 	);
 };
