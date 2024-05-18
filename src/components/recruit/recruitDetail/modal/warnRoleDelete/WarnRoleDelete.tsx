2fcb17ace1b3e223f01987421092c0dda52a40d8
prgmr99 <johnyeom24@naver.com>
2024-05-18 16:01:34 +0900

    fix: #200 역할 삭제 모달 ux 라이팅 및 기능 변경

--- src/components/recruit/recruitDetail/modal/warnRoleDelete/WarnRoleDelete.tsx
+++ src/components/recruit/recruitDetail/modal/warnRoleDelete/WarnRoleDelete.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import S from './WarnRoleDelete.styled';
 import { useSetRecoilState } from 'recoil';
 import { warnRoleDeleteModalState } from '../../../../../atom';
-import { useNavigate } from 'react-router-dom';
 
 const WarnRoleDelete = ({ id }: { id: string }) => {
-	const navigate = useNavigate();
 	const setWarnRoleDeleteState = useSetRecoilState(warnRoleDeleteModalState);
-	const onClickBack = () => {
-		setWarnRoleDeleteState(false);
-	};
 
 	const onClickConfirm = () => {
-		navigate(`/recruitment/applicants/${id}`);
+		// navigate(`/recruitment/applicants/${id}`);
 		setWarnRoleDeleteState(false);
 	};
 
@@ -20,16 +15,13 @@ const WarnRoleDelete = ({ id }: { id: string }) => {
 		<S.WarnRoleDelete>
 			<h3>역할을 삭제할 수 없어요!</h3>
 			<section className='description'>
-				<span className='body2-medium'>해당 역할에 신청자가 존재합니다.</span>
-				<span className='body2-medium'>신청자를 거절한 후에 역할을 삭제할 수 있습니다.</span>
-				<span className='body2-medium'>신청자를 거절할까요?</span>
+				<span className='body2-medium'>신청자가 존재하는 역할은</span>
+				<span className='body2-medium'>삭제할 수 없습니다.</span>
+				<span className='body2-medium'>신청자를 승인/거절 후 삭제해주세요.</span>
 			</section>
 			<section className='btn-container'>
-				<button className='cancel btn-txt' onClick={onClickBack}>
-					취소하기
-				</button>
 				<button className='confirm btn-txt' onClick={onClickConfirm}>
-					거절하러 가기
+					확인
 				</button>
 			</section>
 		</S.WarnRoleDelete>
