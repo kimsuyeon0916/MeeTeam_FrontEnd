57c0985ffa496f16ba90da1a9f26482dafe4848c
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-06-04 18:07:08 +0900

    feat: #217 프로필 이미지 조회 API 호출 커스텀 훅 작성

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,8 +1,9 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { readProfile, updateProfile } from '../service';
+import { readProfile, updateProfile, readProfileImage } from '../service';
 
 const profileKeys = {
 	readProfile: (userId: string) => ['readProfile', userId],
+	readProfileImage: ['readProfileImage'] as const,
 };
 
 /**
@@ -36,5 +37,18 @@ export const useUpdateProfile = ({
 				onSuccess?.(data);
 			}
 		},
+	});
+};
+
+/**
+ * @description 유저 프로필 이미지 조회 API를 호출하는 hook입니다.
+ */
+export const useReadProfileImage = (isLogin: boolean) => {
+	return useQuery({
+		queryKey: profileKeys.readProfileImage,
+		queryFn: () => readProfileImage(),
+		enabled: isLogin,
+		gcTime: Infinity,
+		staleTime: Infinity,
 	});
 };
