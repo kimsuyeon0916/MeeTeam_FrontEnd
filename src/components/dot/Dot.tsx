69b8c4a7e82fb97e18733df3884dcad300c983f8
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2024-08-25 04:24:23 +0000

    delete: #244 사용하지 않는 파일 제거 및 일부 수정 사항

--- src/components/dot/Dot.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import S from './Dot.styled';
-
-const Dot = () => {
-	return <S.Dot>*</S.Dot>;
-};
-
-export default React.memo(Dot);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,84 +1,17 @@
 import Header from './header/Header';
-import Subtitle from './title/subtitle/Subtitle';
-import Dot from './dot/Dot';
-import CustomSelect from './customSelect/CustomSelect';
 import MeeteamTag from './tag/MeeteamTag';
-import AddButton from './addButton/AddButton';
-import ProgressBar from './meeteam/progressBar/ProgressBar';
-import Menu from './meeteam/menu/Menu';
-import Main from './meeteam/main/Main';
-import Information from './meeteam/main/information/Information';
-import Member from './meeteam/main/member/Member';
-import Setting from './meeteam/main/setting/Setting';
-import DashBoard from './meeteam/main/dashBoard/DashBorad';
-import Recruitment from './meeteam/main/recruitment/Recruitment';
-import RequiredInformation from './meeteam/requiredInformation/RequiredInformation';
-import ProgressStatus from './meeteam/progressStatus/ProgressStatus';
-import Title from './meeteam/title/Title';
-import Issue from './meeteam/main/issue/Issue';
-import { issueList, ISSUE_RIGHT_ARROW_ICON } from './meeteam/main/issue/IssueData';
-import {
-	statusList,
-	STATUS_DONE_ICON,
-	STATUS_CLOSE_ICON,
-	STATUS_RIGHT_ARROW_ICON,
-} from './meeteam/progressStatus/StatusData';
-import MeeteamLink from './meeteam/main/link/Link';
-import DashBoardLink from './meeteam/main/dashBoard/dashBoardLink/DashBoardLink';
-import {
-	memberList,
-	MEMBER_PLUS_ICON,
-	MEMBER_PLUS_CARD,
-	MEMBER_BOTTOM_ARROW_ICON,
-	MemberTest,
-} from './meeteam/main/member/MemberData';
-import MemberInviteModal from './meeteam/memberInviteModal/MemberInviteModal';
-import type { MeeTeamMember } from './meeteam/main/member/MemberData';
-import {
-	meeteamInformation,
-	recruitmentInformation,
-	BOTTOM_ARROW_ICON,
-	TOP_ARROW_BUTTON,
-	informationList,
-	role,
-	CONTENT,
-} from './meeteam/main/information/InformationData';
-import MemberCard from './meeteam/memberCard/MemberCard';
-import MemberContact from './meeteam/memberContact/MemberContact';
-import MeeteamInformation from './meeteam/main/information/subInformation/MeeteamInformation';
-import RecruitmentInformation from './meeteam/main/information/subInformation/RecruitmentInformation';
-import ApplicantView from './meeteam/main/recruitment/applicantView/ApplicantView';
-import {
-	applicantList,
-	SMALL_BOTTOM_ARROW_ICON,
-	SMALL_TOP_ARROW_BUTTON,
-} from './meeteam/main/recruitment/applicantView/ApplicantViewData';
-import type { Applicant } from './meeteam/main/recruitment/applicantView/ApplicantViewData';
-import RecruitmentDeadLine from './meeteam/main/recruitment/RecruitmentDeadLine';
 import Toggle from './toggle/Toggle';
 import GoBack from './goBack/GoBack';
 import KebabMenu from './kebabMenu/KebabMenu';
 import KebabMenuIcon from './kebabMenuIcon/KebabMenu';
 import OptionMenu from './optionMenu/OptionMenu';
 import ProfileImage from './profile/image/ProfileImage';
-import {
-	linkList,
-	LINK_BOTTOM_ARROW_ICON,
-	LINK_SHORTCUTS_BUTTON,
-} from './meeteam/main/link/LinkData';
-import EssentialInformation from './meeteam/main/information/subInformation/EssentialInformation';
 import Sidebar from './sidebar/Sidebar';
-import Card from './meeteam/card/Card';
-import PortpolioCard from './meeteam/card/PortpolioCard';
 import Tag from './meeteam/tag/Tag';
-import ManageRecruitCard from './recruit/ManageRecruitCard';
-import Status from './meeteam/status/Status';
 import Dropdown from './dropdown/Dropdown';
-import Icon from './meeteam/icon/Icon';
 import ApplyModal from './recruit/recruitDetail/modal/ApplyModal';
 import ConfirmModal from './recruit/recruitDetail/modal/ConfirmModal';
 import FinalModal from './recruit/recruitDetail/modal/FinalModal';
-import Content from './meeteam/Content';
 import RecruitCard from './meeteam/card/RecruitCard';
 import Pagination from './pagination/Pagination';
 import NaverLogin from './naver/NaverLogin';
@@ -156,75 +89,17 @@ import FinalValidationModal from './recruit/create/modal/finalValidationModal.ts
 export {
 	Header,
 	Footer,
-	Subtitle,
-	Dot,
-	CustomSelect,
 	MeeteamTag,
-	ProgressBar,
-	Menu,
-	Main,
-	Information,
-	Member,
-	Setting,
-	DashBoard,
-	Recruitment,
-	RequiredInformation,
-	ProgressStatus,
-	Title,
-	Issue,
-	issueList,
-	ISSUE_RIGHT_ARROW_ICON,
-	statusList,
-	STATUS_DONE_ICON,
-	STATUS_RIGHT_ARROW_ICON,
-	STATUS_CLOSE_ICON,
-	MeeteamLink,
-	DashBoardLink,
-	memberList,
-	MeeTeamMember,
-	MEMBER_PLUS_ICON,
-	MEMBER_PLUS_CARD,
-	MEMBER_BOTTOM_ARROW_ICON,
-	meeteamInformation,
-	recruitmentInformation,
-	BOTTOM_ARROW_ICON,
-	TOP_ARROW_BUTTON,
-	MemberCard,
-	MemberContact,
-	MeeteamInformation,
-	RecruitmentInformation,
-	ApplicantView,
-	applicantList,
-	SMALL_BOTTOM_ARROW_ICON,
-	SMALL_TOP_ARROW_BUTTON,
-	Applicant,
-	RecruitmentDeadLine,
 	Toggle,
 	GoBack,
 	KebabMenu,
 	KebabMenuIcon,
 	OptionMenu,
 	ProfileImage,
-	linkList,
-	LINK_BOTTOM_ARROW_ICON,
-	LINK_SHORTCUTS_BUTTON,
-	EssentialInformation,
-	AddButton,
 	Sidebar,
-	Card,
 	Tag,
-	ManageRecruitCard,
-	Status,
 	Dropdown,
-	Icon,
-	MemberInviteModal,
-	MemberTest,
 	NaverLogin,
-	Content,
-	PortpolioCard,
-	informationList,
-	role,
-	CONTENT,
 	RecruitCard,
 	Pagination,
 	CommentInput,
